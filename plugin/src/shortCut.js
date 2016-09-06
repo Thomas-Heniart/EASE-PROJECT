f5a8dbd01ea690a1999c7bd31470a8f59da5c3e9
felix <fel.richart@gmail.com>
2016-09-06 18:53:40 +0200

    json++

--- plugin/src/shortCut.js
+++ plugin/src/shortCut.js
@@ -1,12 +1,11 @@
 extension.addShortCut(function(command){
     if(command == "open-ease") {
         window.open('http://ease.space');
-        //nothing to say
     }
 });
 
-extension.onWindowCreated(function() {
+/*extension.onWindowCreated(function() {
 	if (extension.storage.get("NewTab") == "1"){
  	   window.open('http://ease.space');
 	}
-});
+});*/
