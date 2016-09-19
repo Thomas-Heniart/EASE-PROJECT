3373ad475b82fb0d06b5e72a7aacda7fb19383e0
felix <fel.richart@gmail.com>
2016-09-19 14:20:21 +0200

    ok

--- plugin/src/actions.js
+++ plugin/src/actions.js
@@ -1,20 +1,24 @@
 var actions = {
 erasecookies:function(msg, callback, sendResponse){
     
-    function deleteAllCookies() {
-    var cookies = document.cookie.split(";");
-    for (var i = 0; i < cookies.length; i++) {
-    	var cookie = cookies[i];
-    	var eqPos = cookie.indexOf("=");
-    	var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    var name = msg.detail[msg.bigStep].website[msg.todo].todo[msg.actionStep].search;
+    
+    function deleteCookie(cookieName){
         var domain = document.domain;
         var path = "/";
-    	document.cookie = name + "=; expires=" + +new Date + "; domain=" + domain + "; path=" + path;
-    }
-    
+    	document.cookie = cookieName + "=; expires=" + +new Date + "; domain=" + domain + "; path=" + path;
     }
 
-    deleteAllCookies();
+    if(name) {deleteCookie(name)}
+    else {
+        var cookies = document.cookie.split(";");
+        for (var i = 0; i < cookies.length; i++) {
+             var cookie = cookies[i];
+             var eqPos = cookie.indexOf("=");
+             var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+            deleteCookie(name)
+        }
+    }
     msg.actionStep++;
     callback(msg, sendResponse);
     
