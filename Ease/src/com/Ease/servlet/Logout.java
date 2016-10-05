7bbf78fed6e3b866a8bdc064a8ab29239396ca38
debruy_p <pierre.debruyne@epitech.eu>
2016-10-05 18:31:37 +0200

    logout

--- Ease/src/com/Ease/servlet/Logout.java
+++ Ease/src/com/Ease/servlet/Logout.java
@@ -1,6 +1,7 @@
 package com.Ease.servlet;
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -31,6 +32,12 @@ public class Logout extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("logout.jsp");
+		rd.forward(request, response);
+	}
+    
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		HttpSession session = request.getSession();
