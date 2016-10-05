19cde08a07a5e3335c2fe67fab5a28c818ef5e96
debruy_p <pierre.debruyne@epitech.eu>
2016-10-05 17:35:41 +0200

    correctif

--- Ease/src/com/Ease/servlet/Logout.java
+++ Ease/src/com/Ease/servlet/Logout.java
@@ -31,7 +31,7 @@ public class Logout extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		HttpSession session = request.getSession();
 		User user = (User)(session.getAttribute("User"));
