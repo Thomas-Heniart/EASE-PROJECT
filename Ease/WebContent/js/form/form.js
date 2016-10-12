d2338dc68b3773f4601319c6324c4b5a48c2ead3
debruy_p <pierre.debruyne@epitech.eu>
2016-10-12 14:44:21 +0200

    first object

---
+++ Ease/WebContent/js/form/form.js
@@ -0,0 +1,38 @@
+
+var From = function (rootEl, inputs, button) {
+	var self = this;
+	this.qRoot = rootEl;
+	this.qInputs = inputs;
+	this.qButton = button;
+	this.isEnabled = false;
+	for (var i = 0; i < this.qInputs.length; ++i) {
+		this.qInputs[i].listenBy(this.qRoot);
+	}
+	this.enable = function () {
+		self.isEnabled = true;
+		self.qButton.prop('disabled', false);
+		self.qButton.addClass("Active");
+	};
+	this.disable = function () {
+		self.isEnabled = false;
+		self.qButton.prop('disabled', true);
+		self.qButton.removeClass("Active");
+	};
+	this.qRoot.on("StateChanged", function () {
+		self.checkInputs() && self.enable() || self.disable();
+	});
+	this.checkInputs = function () {
+		for (var i = 0; i < self.qInputs.length; ++i) {
+			if (self.qInput[i].isValid == false) {
+				return false;
+			}
+		}
+		return true;
+	};
+	this.reset = function () {
+		self.disable();
+		for (var i = 0; i < self.qInputs.length; ++i) {
+			self.qInputs[i].reset();
+		}
+	};
+}
---
+++ Ease/WebContent/js/form/input.js
@@ -0,0 +1,74 @@
+
+
+var Input = function (rootEl) {
+	var self = this;
+	this.qInput = rootEl;
+	this.listeners = [];
+	this.listenBy = function (qBy) {
+		self.listeners.push(qBy);
+	}
+	this.isValid = false;
+	this.validate = function () {
+		return (self.qInput.val().length > 0);
+	};
+	this.onStateChanged = function () {
+		for (var i = 0; i < self.target.length; ++i) {
+			self.listeners[i].trigger("StateChanged");
+		}
+		if (seld.isValid) {
+			self.qInput.removeClass("unValid");
+			self.qInput.addClass("valid");
+		} else {
+			self.qInput.removeClass("valid");
+			self.qInput.addClass("unValid");
+		}
+	};
+	this.reset = function () {
+		self.qInput.val("");
+		self.qInput.removeClass("valid");
+		self.qInput.removeClass("unValid");
+		self.isValid = false;
+	};
+	this.val = function (value) {
+		self.qInput.val(value);
+		var tmp = self.validate();
+		if (tmp != self.isValid) {
+			self.isValid = tmp;
+			self.onStateChanged();
+		}
+	};
+	this.qInput.on("keyup", function () {
+		var tmp = self.validate();
+		if (tmp != self.isValid) {
+			self.isValid = tmp;
+			self.onStateChanged();
+		}
+	});
+}
+
+var EmailInput = function (rootEl) {
+	Input.apply(this,arguments);
+	var self = this;
+	this.validate = function () {
+		var reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	    return reg.test(self.qInput.val());
+	}
+}
+
+var PasswordInput = function (rootEl) {
+	Input.apply(this,arguments);
+	var self = this;
+	this.validate = function () {
+		var reg = /^[A-Za-z0-9$@$!%.*#?&'()-_<>=+\\/:;,]{8,}$/;
+	    return reg.test(self.qInput.val());
+	}
+}
+
+var UrlInput = function (rootEl) {
+	Input.apply(this,arguments);
+	var self = this;
+	this.validate = function () {
+		var reg = /^[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
+	    return reg.test(self.qInput.val());
+	}
+}
