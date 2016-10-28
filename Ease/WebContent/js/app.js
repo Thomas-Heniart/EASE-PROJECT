9664efd5d912eec98915a00e7f50c75de6b66e77
debruy_p <pierre.debruyne@epitech.eu>
2016-10-28 10:32:37 +0200

    merge

--- Ease/WebContent/js/app.js
+++ Ease/WebContent/js/app.js
@@ -1,4 +1,3 @@
-var easeApps = [];
 
 var easeApp = function(rootEl){
 	var self = this;
@@ -8,7 +7,7 @@ var easeApp = function(rootEl){
 	this.name = this.rootEl.attr('name');
 	this.id = this.rootEl.attr('id');
 	this.webId = this.rootEl.attr('webid');
-
+	this.login = this.rootEl.attr('login');
 	this.setName = function(name){
 		self.name = name;
 		self.nameArea.text(name);
@@ -31,5 +30,4 @@ var easeApp = function(rootEl){
 	this.rootEl.find('.deleteAppButton').length && this.rootEl.find('.deleteAppButton').on('click', function(e){
 		showConfirmDeleteAppPopup(this, e);
 	});
-	easeApps.push(this);
 }
