688a77e9d49366cb0248520c8946f39cfd5ff936
felix <fel.richart@gmail.com>
2016-10-28 10:40:32 +0200

    ntm piere

--- plugin/build.cmd
+++ plugin/build.cmd
@@ -2,10 +2,6 @@ xcopy /i src out\chrome /s
 xcopy /i src out\firefox /s
 xcopy /i src out\safari /s
 
-xcopy /i nav\chrome\chrome.js out\chrome\extension.js
-xcopy /i nav\chrome\manifest.json out\chrome\manifest.json
-
-xcopy /i nav\safari\safari.js out\safari\extension.js
-
-xcopy /i nav\firefox\firefox.js out\firefox\extension.js
-xcopy /i nav\firefox\manifest.json out\firefox\manifest.json
+xcopy /i nav\chrome out\chrome /s
+xcopy /i nav\firefox out\firefox /s
+xcopy /i nav\safari out\safari /s
