8f0a63b302bdc40839e5cb7c276d03d211f87d55
felix <fel.richart@gmail.com>
2016-11-17 20:41:35 +0100

    ok

--- tools/procedures.sql
+++ tools/procedures.sql
@@ -1,12 +1,10 @@
-DROP PROCEDURE IF EXISTS addEmail;
-DROP PROCEDURE IF EXISTS deleteUser;
 DELIMITER //
 
 CREATE PROCEDURE addEmail(IN p_user_id INT(10) UNSIGNED, IN p_email VARCHAR(100))
 BEGIN
   SELECT count(id) INTO @var FROM usersEmails WHERE user_id = p_user_id AND email = p_email;
   IF @var = 0 THEN
-    INSERT INTO usersEmails values (NULL, p_user_id, p_email, 0, NULL);
+    INSERT INTO usersEmails values (NULL, p_user_id, p_email, 0);
   END IF;
 END //
 
@@ -32,3 +30,49 @@ BEGIN
 END //
 
 DELIMITER ;
+
+DELIMITER //
+
+CREATE PROCEDURE increaseRatio(IN p_website_id INT(10) UNSIGNED)
+BEGIN
+  UPDATE websites SET ratio = ratio + 1 WHERE website_id = p_website_id;
+END //
+
+CREATE PROCEDURE decreaseRatio(IN p_website_id INT(10) UNSIGNED)
+BEGIN
+  UPDATE websites SET ratio = ratio - 1 WHERE website_id = p_website_id;
+END //
+
+CREATE PROCEDURE setRatio(IN p_website_id INT(10) UNSIGNED, IN new_ratio INT(10) UNSIGNED)
+BEGIN
+  UPDATE websites SET ratio = new_ratio WHERE website_id = p_website_id;
+END //
+
+CREATE PROCEDURE lockWebsite(IN p_website_id INT(10) UNSIGNED)
+BEGIN
+  START TRANSACTION;
+  SELECT position INTO @var FROM websites WHERE website_id = p_website_id;
+  SET @i=2;
+  UPDATE websites SET position = (@i:=@i+1) WHERE position < @var ORDER BY position;
+  UPDATE websites SET locked = 1, position = 2, lockedExpiration = CURDATE() + INTERVAL 3 DAY WHERE website_id = p_website_id;
+  COMMIT;
+END //
+
+CREATE PROCEDURE unlockWebsites()
+BEGIN
+  UPDATE websites SET locked = 0 WHERE lockedExpiration >= CURDATE();
+END //
+
+CREATE PROCEDURE updateWebsitesPositions()
+BEGIN
+  START TRANSACTION;
+  CALL unlockWebsites();
+  UPDATE websites SET position = 1 WHERE insertDate >= CURDATE() - INTERVAL 3 DAY ORDER BY ratio DESC;
+  UPDATE websites SET position = 2 WHERE locked = 1;
+  SET @i=2;
+  UPDATE websites SET position = (@i:=@i+1) WHERE insertDate < CURDATE() - INTERVAL 3 AND locked = 0 DAY ORDER BY ratio DESC;
+  COMMIT;
+END //
+
+DELIMITER ;
+
