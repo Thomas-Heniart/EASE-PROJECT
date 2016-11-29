dd5e20578bfc6a0e8ce385959b5fcbf388fa44b8
Thomas Heniart <heniart.thomas@gmail.com>
2016-11-29 17:30:46 +0100

    invitation

--- Ease/src/com/Ease/websocket/Invitation.java
+++ Ease/src/com/Ease/websocket/Invitation.java
@@ -1,5 +1,18 @@
 package com.Ease.websocket;
 
+import com.Ease.utils.CodeGenerator;
+import com.Ease.utils.DataBaseConnection;
+import com.Ease.utils.GeneralException;
+import com.Ease.utils.ServletManager;
+
 public class Invitation {
 
+	public static void createInvitation(String email, String groupId, ServletManager sm) throws GeneralException {
+		DataBaseConnection db = sm.getDB();
+		String invitationCode = CodeGenerator.generateNewCode();
+		int transaction = db.startTransaction();
+		
+		db.commitTransaction(transaction);
+	}
+	
 }
