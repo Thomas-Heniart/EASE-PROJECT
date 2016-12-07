4e1b28ca2b408a7f953d4d3400d64ace469864d8
Thomas Heniart <heniart.thomas@gmail.com>
2016-12-07 10:55:36 +0100

    groupLinkApp load

--- Ease/src/com/Ease/Context/Group/GroupLinkApp.java
+++ Ease/src/com/Ease/Context/Group/GroupLinkApp.java
@@ -10,26 +10,24 @@ import com.Ease.Utils.GeneralException;
 import com.Ease.Utils.ServletManager;
 
 public class GroupLinkApp extends GroupApp {
-
-	protected LinkAppInformation link_app_informations;
-
-	public GroupLinkApp(String db_id, GroupProfile groupProfile, Group group, AppPermissions permissions, String type,
-			AppInformation app_informations, LinkAppInformation link_app_informations, boolean common) {
-		super(db_id, groupProfile, group, permissions, type, app_informations, common);
-		this.link_app_informations = link_app_informations;
-	}
 	
-	public GroupLinkApp(String db_id, ServletManager sm) throws GeneralException {
-		super (db_id, sm);
+	public static GroupLinkApp loadGroupLinkApp(String db_id, Group group, GroupProfile groupProfile, AppPermissions permissions, AppInformation informations, boolean common, ServletManager sm) throws GeneralException {
 		DataBaseConnection db = sm.getDB();
-		ResultSet rs = db.get("SELECT link_app_info_id FROM groupLinkApps WHERE group_app_id = " + db_id  + ";");
+		ResultSet rs = db.get("SELECT link_app_info_id FROM groupLinkApps WHERE group_app_id = " + db_id + ";");
 		try {
 			rs.next();
-			this.link_app_informations = LinkAppInformation.loadLinkAppInformation(rs.getString(1), sm);
+			LinkAppInformation linkAppInformations = LinkAppInformation.loadLinkAppInformation(db_id, sm);
+			return new GroupLinkApp(db_id, groupProfile, group, permissions, informations, linkAppInformations, common);
 		} catch (SQLException e) {
 			throw new GeneralException(ServletManager.Code.InternError, e);
 		}
-		
+	}
+	
+	protected LinkAppInformation link_app_informations;
+
+	public GroupLinkApp(String db_id, GroupProfile groupProfile, Group group, AppPermissions permissions, AppInformation app_informations, LinkAppInformation link_app_informations, boolean common) {
+		super(db_id, groupProfile, group, permissions, app_informations, common);
+		this.link_app_informations = link_app_informations;
 	}
 
 	public LinkAppInformation getLinkAppInfo() {
