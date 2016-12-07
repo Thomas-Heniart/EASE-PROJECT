86d332d728d75919c0e8c342d2b3685b18c3d88f
Thomas Heniart <heniart.thomas@gmail.com>
2016-12-07 14:24:37 +0100

    tag

--- Ease/src/com/Ease/Dashboard/App/Tag.java
+++ Ease/src/com/Ease/Dashboard/App/Tag.java
@@ -1,12 +1,17 @@
 package com.Ease.Dashboard.App;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Tag {
 	
 	protected String tagName;
 	protected String tagColor;
+	protected List<Website> tagSites;
 	
 	public Tag(String tagName, String tagColor) {
 		this.tagName = tagName;
 		this.tagColor = tagColor;
+		this.tagSites = new LinkedList<Website> ();
 	}
 }
