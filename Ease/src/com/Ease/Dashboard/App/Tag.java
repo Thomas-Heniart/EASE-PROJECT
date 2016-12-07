eae11b0a9402f9cc3606c82a5599deefcd1c82cc
Thomas Heniart <heniart.thomas@gmail.com>
2016-12-07 14:22:48 +0100

    Tag class

---
+++ Ease/src/com/Ease/Dashboard/App/Tag.java
@@ -0,0 +1,12 @@
+package com.Ease.Dashboard.App;
+
+public class Tag {
+	
+	protected String tagName;
+	protected String tagColor;
+	
+	public Tag(String tagName, String tagColor) {
+		this.tagName = tagName;
+		this.tagColor = tagColor;
+	}
+}
