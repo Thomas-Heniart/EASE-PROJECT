f58f0896132218e7e39d88c59a05af7e0b6f3c5a
Thomas Heniart <heniart.thomas@gmail.com>
2016-12-07 14:26:22 +0100

    tag constructor

--- Ease/src/com/Ease/Dashboard/App/Tag.java
+++ Ease/src/com/Ease/Dashboard/App/Tag.java
@@ -5,11 +5,15 @@ import java.util.List;
 
 public class Tag {
 	
+	protected String db_id;
+	protected int single_id;
 	protected String tagName;
 	protected String tagColor;
 	protected List<Website> tagSites;
 	
-	public Tag(String tagName, String tagColor) {
+	public Tag(String db_id, int single_id, String tagName, String tagColor, List<Website> tagSites) {
+		this.db_id = db_id;
+		this.single_id = single_id;
 		this.tagName = tagName;
 		this.tagColor = tagColor;
 		this.tagSites = new LinkedList<Website> ();
