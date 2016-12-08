646beaf687bafbb8f891b85c5399313ce7eaa799
debruy_p <pierre.debruyne@epitech.eu>
2016-12-08 12:04:05 +0100

    Test password

--- Ease/src/com/Ease/Dashboard/App/WebsiteApp/ClassicApp/Account.java
+++ Ease/src/com/Ease/Dashboard/App/WebsiteApp/ClassicApp/Account.java
@@ -2,6 +2,10 @@ package com.Ease.Dashboard.App.WebsiteApp.ClassicApp;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 import com.Ease.Dashboard.User.User;
 import com.Ease.Utils.DataBaseConnection;
@@ -16,20 +20,20 @@ public class Account {
 		SHARED
 	}
 	
-	public static Account loadAccount(String db_id, User user, ServletManager sm) throws GeneralException {
+	public static Account loadAccount(String db_id, ServletManager sm) throws GeneralException {
 		DataBaseConnection db = sm.getDB();
 		ResultSet rs = db.get("SELECT * FROM accounts WHERE id = " + db_id + ";");
 		try {
 			if (rs.next()) {
 				String password = rs.getString(Data.PASSWORD.ordinal());
+				List<AccountInformation> infos = AccountInformation.loadInformations(db_id, sm);
 				boolean shared = rs.getBoolean(Data.SHARED.ordinal());
-				return new Account(db_id, user.decrypt(password), shared);
+				return new Account(db_id, password, shared, infos);
 			}
 		} catch (SQLException e) {
-			e.printStackTrace();
 			throw new GeneralException(ServletManager.Code.InternError, e);
 		}
-		return null;
+		throw new GeneralException(ServletManager.Code.InternError, "This account dosen't exist.");
 	}
 	
 	public static Account createAccount(String password, boolean shared, User user, ServletManager sm) throws GeneralException {
@@ -38,22 +42,16 @@ public class Account {
 		return new Account(String.valueOf(db_id), password, shared);
 	}
 	
-	protected String db_id;
-	protected String password;
-	protected boolean shared;
+	protected String 				db_id;
+	protected String 				password;
+	protected boolean 				shared;
+	protected List<AccountInformation>	infos;
 	
-	public Account(String db_id, String password, boolean shared) {
+	public Account(String db_id, String password, boolean shared, List<AccountInformation> infos) {
 		this.db_id = db_id;
 		this.password = password;
 		this.shared = shared;
+		this.infos = infos;
 	}
 	
-	public String getDb_id() {
-		return this.db_id;
-	}
-	
-	public void removeFromDb(ServletManager sm) throws GeneralException {
-		DataBaseConnection db = sm.getDB();
-		db.set("DELETE FROM accounts WHERE id=" + this.db_id + ";");
-	}
 }
