e9725cb30fe989cfbf1ff63a12502b20432a5ff3
Thomas Heniart <heniart.thomas@gmail.com>
2017-01-11 14:53:48 +0100

    push variables

--- Ease/src/com/Ease/Context/Variables.java
+++ Ease/src/com/Ease/Context/Variables.java
@@ -11,19 +11,19 @@ public class Variables {
 	*/
 	
 	//Préprod
-	/*
 	public static final String ENVIRONNEMENT = "Préprod";
 	public static final boolean LOCAL = false;
-	public static final String SERVER_LOGIN_PATH = "/var/lib/tomcat/webapps/serverLogin";
-	public static final String KEYS_PATH = "/var/lib/tomcat/webapps/ROOT/WEB-INF/classes/com/Ease/Utils/Crypto/privateKeys.txt";
-	*/
-	
+	public static final String SERVER_LOGIN_PATH = "/opt/tomcat/webapps/serverLogin";
+	public static final String KEYS_PATH = "/opt/tomcat/webapps/ROOT/WEB-INF/classes/com/Ease/Utils/Crypto/privateKeys.txt";
+	public static final String WEBSITES_PATH = "/resources/websites/";
 	//Féfé
+	/*
 	public static final String ENVIRONNEMENT = "PC de Féfé";
 	public static final boolean LOCAL = true;
 	public static final String SERVER_LOGIN_PATH = "";
 	public static final String KEYS_PATH = "C:/Users/FelixPro/Documents/EASE/Ease project/New/EASE-PROJECT/Ease/src/com/Ease/Utils/Crypto/privateKeys.txt";
 	public static final String WEBSITES_PATH = "C:/Users/FelixPro/Documents/EASE/Ease project/New/EASE-PROJECT/Ease/WebContent/resources/websites/";
+	*/
 	
 	//Pierre
 	/*
@@ -48,5 +48,6 @@ public class Variables {
 	public static final boolean LOCAL = true;
 	public static final String SERVER_LOGIN_PATH = "";
 	public static final String KEYS_PATH = "/home/thomas/EASE-PROJECT/Ease/src/com/Ease/Utils/Crypto/privateKeys.txt";
+	public static final String WEBSITES_PATH = "/home/thomas/EASE-PROJECT/Ease/WebContent/resources/websites/";
 	*/
 }
