bfbbb0ef91255d956f4eaaec98961f0582e466cc
Thomas Heniart <heniart.thomas@gmail.com>
2017-01-16 12:57:54 +0100

    sprint updates
    updates loader

---
+++ Ease/src/com/Ease/Update/Update.java
@@ -0,0 +1,69 @@
+package com.Ease.Update;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.Ease.Dashboard.User.User;
+import com.Ease.Utils.DataBaseConnection;
+import com.Ease.Utils.GeneralException;
+import com.Ease.Utils.ServletManager;
+
+public class Update {
+	
+	public enum Data {
+		NOTHING,
+		ID,
+		USER_ID,
+		TYPE
+	}
+	
+	public static List<Update> loadUpdates(User user, ServletManager sm) throws GeneralException {
+		DataBaseConnection db = sm.getDB();
+		List<Update> updates = new LinkedList<Update>();
+		try {
+			ResultSet rs = db.get("SELECT * FROM updates WHERE user_id = " + user.getDBid() + ";");
+			try {
+				String db_id;
+				String type;
+				Update update;
+				while (rs.next()) {
+					db_id = rs.getString(Data.ID.ordinal());
+					type = rs.getString(Data.TYPE.ordinal());
+					switch (type) {
+					case "updateNewPassword":
+						update = UpdateNewPassword.loadUpdateNewPassword(db_id, user, sm);
+						break;
+					
+					case "updateNewAccount":
+						update = UpdateNewAccount.loadUpdateNewAccount(db_id, user, sm);
+						break;
+					
+					default:
+						throw new GeneralException(ServletManager.Code.InternError, "No such type");
+					}
+					updates.add(update);
+				}
+			} catch (SQLException e) {
+				throw new GeneralException(ServletManager.Code.InternError, e);
+			}
+			return updates;
+		} catch(GeneralException e) {
+			throw e;
+		}
+		
+	}
+	
+	protected String db_id;
+	protected String type;
+	protected int single_id;
+	protected User user;
+	
+	public Update(String db_id, int single_id) {
+		this.db_id = db_id;
+		//this.type = type;
+		this.single_id = single_id;
+		//this.user = user;
+	}
+}
---
+++ Ease/src/com/Ease/Update/UpdateNewPassword.java
@@ -0,0 +1,46 @@
+package com.Ease.Update;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.Ease.Dashboard.App.WebsiteApp.ClassicApp.ClassicApp;
+import com.Ease.Dashboard.User.User;
+import com.Ease.Utils.DataBaseConnection;
+import com.Ease.Utils.GeneralException;
+import com.Ease.Utils.IdGenerator;
+import com.Ease.Utils.ServletManager;
+
+public class UpdateNewPassword extends Update {
+
+	public enum Data {
+		NOTHING, 
+		ID, 
+		CLASSIC_APP_ID, 
+		NEW_PASSWORD
+	}
+
+	public static Update loadUpdateNewPassword(String update_id, User user, ServletManager sm) throws GeneralException {
+		DataBaseConnection db = sm.getDB();
+		IdGenerator idGenerator = (IdGenerator) sm.getContextAttr("idGenerator");
+		ResultSet rs = db.get("SELECT * FROM updateNewPassword WHERE update_id = " + update_id + ";");
+		try {
+			rs.next();
+			ClassicApp classicApp = (ClassicApp) user.getAppWithDBid(rs.getString(Data.CLASSIC_APP_ID.ordinal()));
+			String newPassword = rs.getString(Data.NEW_PASSWORD.ordinal());
+			return new UpdateNewPassword(update_id, classicApp, newPassword, idGenerator.getNextId());
+		} catch (SQLException e) {
+			throw new GeneralException(ServletManager.Code.InternError, e);
+		}
+	}
+
+	protected String newPassword;
+	protected ClassicApp classicApp;
+
+	public UpdateNewPassword(String db_id, ClassicApp classicApp, String newPassword, int single_id) {
+		super(db_id, single_id);
+		this.classicApp = classicApp;
+		this.newPassword = newPassword;
+
+	}
+
+}
