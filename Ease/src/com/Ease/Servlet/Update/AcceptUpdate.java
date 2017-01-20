1e32a96e93abdd2da8150620f3493690b4c5fc42
debruy_p <pierre.debruyne@epitech.eu>
2017-01-20 19:58:58 +0100

    Update crash test n1

---
+++ Ease/src/com/Ease/Servlet/Update/AcceptUpdate.java
@@ -0,0 +1,65 @@
+package com.Ease.Servlet.Update;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.Ease.Dashboard.User.User;
+import com.Ease.Utils.GeneralException;
+import com.Ease.Utils.ServletManager;
+
+/**
+ * Servlet implementation class AcceptUpdate
+ */
+@WebServlet("/AcceptUpdate")
+public class AcceptUpdate extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AcceptUpdate() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		User user = (User) (session.getAttribute("user"));
+		ServletManager sm = new ServletManager(this.getClass().getName(), request, response, true);
+		
+		try {
+			sm.needToBeConnected();
+			String updateId = sm.getServletParam("updateId", true);
+			String profileId = sm.getServletParam("profileId", true);
+			String password = sm.getServletParam("password", true);
+			user.getUpdateManager().acceptUpdate(Integer.parseInt(updateId), ((profileId == null ) ? -1 : Integer.parseInt(profileId)), password, sm);
+			sm.setResponse(ServletManager.Code.Success, "Update added.");
+		} catch (GeneralException e) {
+			sm.setResponse(e);
+		} catch (NumberFormatException e) {
+			sm.setResponse(ServletManager.Code.ClientError, "Wrong numbers.");
+		} catch (Exception e) {
+			sm.setResponse(e);
+		}
+		sm.sendResponse();
+	}
+
+}
