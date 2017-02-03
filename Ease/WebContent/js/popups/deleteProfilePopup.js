5c185854d5ed4682848a1d7265dbdc0c36dd71ff
Fisun <sergii.fisun@epitech.eu>
2017-02-03 21:00:48 +0100

    popup delete profile

---
+++ Ease/WebContent/js/popups/deleteProfilePopup.js
@@ -0,0 +1,71 @@
+deleteProfilePopup = function(rootEl){
+	var self = this;
+	this.qRoot = $(rootEl);
+	this.parentHandler = this.qRoot.closest('#easePopupsHandler');
+
+	this.errorRowHandler = this.qRoot.find('.errorHandler');
+
+	this.passwordInputHander = this.qRoot.find("input[name='password']");
+
+	this.formHandler = this.qRoot.find("#deleteProfileForm");
+	this.submitButton = this.qRoot.find("button[type='submit']");
+	this.goBackButtonHandler = this.qRoot.find("#goBack");
+
+	this.goBackButtonHandler.click(function(){
+		self.close();
+	});
+	this.passwordInputHander.on('input', function(){
+		if ($(this).val().length)
+			self.submitButton.removeClass('locked');
+		else
+			self.submitButton.addClass('locked');
+	});
+	this.formHandler.submit(function(e){
+		e.preventDefault();
+		if (!(self.passwordInputHander.val().length))
+			return;
+		self.errorRowHandler.removeClass('show');
+		self.submitButton.addClass('loading');
+		postHandler.post(
+			'RemoveProfile',
+			{
+				profileId: self.currentProfile.id,
+				password: self.passwordInputHander.val()
+			},
+			function(){
+				self.submitButton.removeClass('loading');
+			},
+			function(msg){
+				easeTracker.trackEvent('DeleteProfile');
+				self.currentProfile.remove();
+				self.close();
+			},
+			function(msg){
+				self.errorRowHandler.find('p').text(msg);
+				self.errorRowHandler.addClass('show');
+			},
+			'text'
+		);
+	});
+	this.currentProfile = null;
+	this.reset = function(){
+		self.passwordInputHander.val('');
+		self.submitButton.addClass('locked');
+		self.errorRowHandler.removeClass('show');
+	}
+	this.open = function(profile){
+		self.reset();
+		self.currentProfile = profile;
+		self.parentHandler.addClass('myshow');
+		self.qRoot.addClass('show');
+	}
+	this.close = function(){
+		self.qRoot.removeClass('show');
+		self.parentHandler.removeClass('myshow');
+	}
+}
+
+var easeDeleteProfilePopup;
+$(document).ready(function(){
+	easeDeleteProfilePopup = new deleteProfilePopup($('#deleteProfilePopup'));
+});
--- Ease/WebContent/templates/PopupsHandler.jsp
+++ Ease/WebContent/templates/PopupsHandler.jsp
@@ -4,5 +4,6 @@ pageEncoding="UTF-8"%>
 <div class="popupHandler" id="easePopupsHandler">
 	<%@ include file="popups/addUpdatePopup.jsp" %>
 	<%@ include file="popups/addAppPopup.jsp" %>
-	<%@ include file="popups/addBookmarkPopup.jsp" %>	
+	<%@ include file="popups/addBookmarkPopup.jsp" %>
+	<%@ include file="popups/deleteProfilePopup.jsp" %>		
 </div>
