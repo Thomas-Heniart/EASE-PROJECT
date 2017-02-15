cf916a60a974c92375eb6123ba46204d1c5d92a2
debruy_p <pierre.debruyne@epitech.eu>
2017-02-15 21:58:15 +0100

    bug

--- Ease/src/com/Ease/Servlet/CheckInvitation.java
+++ Ease/src/com/Ease/Servlet/CheckInvitation.java
@@ -78,7 +78,7 @@ public class CheckInvitation extends HttpServlet {
 					rs = db.get("SELECT group_id FROM invitationsAndGroupsMap JOIN invitations ON invitationsAndGroupsMap.invitation_id = invitations.id WHERE email='" + email + "';");
 					if (rs.next()) {
 						Invitation.sendInvitation(email, name, null, sm);
-						sm.setResponse(ServletManager.Code.Success, "1 You receveid an email");
+						sm.setResponse(ServletManager.Code.Success, "1 You received an email");
 					} else {
 						sm.setResponse(ServletManager.Code.Success, "2 Go to registration");
 					}
