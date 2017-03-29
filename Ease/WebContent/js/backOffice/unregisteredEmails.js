c2c1d2ebd1e2673756ff6e1799f765368d2d261d
Thomas Heniart <heniart.thomas@gmail.com>
2017-03-29 18:55:34 +0200

    Fix unregistered emails

--- Ease/WebContent/js/backOffice/unregisteredEmails.js
+++ Ease/WebContent/js/backOffice/unregisteredEmails.js
@@ -1,32 +1,35 @@
-postHandler.post("GetUnregisteredEmails", {},
-	function(){ $("#UnregisteredUsersTab #results div").remove() },
-	printEmailsFromJson,
-	function(data) {
+$(document).ready(function() {
+	postHandler.post("GetUnregisteredEmails", {}, function() {
+		$("#UnregisteredUsersTab #results div").remove()
+	}, printEmailsFromJson, function(data) {
 	});
+	
+	$("#UnregisteredUsersTab #results").on("click", ".delete-unregistered-email",
+			function(e) {
+				var self = $(this);
+				var email = self.parent().attr("email");
+				postHandler.post("DeleteUnregisteredEmail", {
+					email : email
+				}, function() {
+				}, function(data) {
+					self.parent().remove();
+				}, function(data) {
+				});
+			});
+});
 
-	function printEmailsFromJson(data) {
-		var json = JSON.parse(data);
-		json.forEach(function(elem) {
-			$("#UnregisteredUsersTab #results").append("<div email='"
-			+ elem.email
-			+ "'>"
-			+ "<i class='fa fa-times delete-unregistered-email'></i>"
-			+ elem.date
-			+ " "
-			+ elem.email
-			+ "</div>");
-		});
-	}
-
-	$("#UnregisteredUsersTab #results").on("click", ".delete-unregistered-email", function(e) {
-		var self = $(this);
-		var email = self.parent().attr("email");
-		postHandler.post("DeleteUnregisteredEmail", {
-			email: email
-		}, function() {
-		},
-		function(data) {
-			self.parent().remove();
-		},
-		function(data){});	
-	});
+function printEmailsFromJson(data) {
+	var json = JSON.parse(data);
+	json
+			.forEach(function(elem) {
+				console.log(elem);
+				$("#UnregisteredUsersTab #results")
+						.append(
+								"<div email='"
+										+ elem.email
+										+ "'>"
+										+ "<i class='fa fa-times delete-unregistered-email'></i>"
+										+ elem.date + " " + elem.email
+										+ "</div>");
+			});
+}
