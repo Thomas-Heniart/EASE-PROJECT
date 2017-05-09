603af29c134d7937e681951daca2800de7e258d5
Thomas Heniart <heniart.thomas@gmail.com>
2017-05-09 09:15:29 +0200

    new ServletManager

--- Ease/src/com/Ease/API/V1/Teams/ServletCreateTeam.java
+++ Ease/src/com/Ease/API/V1/Teams/ServletCreateTeam.java
@@ -78,7 +78,6 @@ public class ServletCreateTeam extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
-        rd.forward(request, response);
+
     }
 }
--- Ease/src/com/Ease/Utils/GeneralException.java
+++ Ease/src/com/Ease/Utils/GeneralException.java
@@ -1,30 +1,54 @@
 package com.Ease.Utils;
 
-public class GeneralException extends Exception{
-	private static final long serialVersionUID = 1L;
-	protected String 	msg;
-	protected ServletManager.Code	code;
+public class GeneralException extends Exception {
+    private static final long serialVersionUID = 1L;
+    protected String msg;
+    protected ServletManager.Code code;
+    protected com.Ease.API.Utils.ServletManager.Code code1;
 
-	public GeneralException(ServletManager.Code code, String msg){
-		this.msg = msg;
-		this.code = code;
-		System.out.println(msg);
-	}
-	
-	public GeneralException(ServletManager.Code code, Exception e){
-		this.msg = e.toString()+".\nStackTrace :";
-		for(int i = 0; i < e.getStackTrace().length; i++){
-			this.msg += "\n" + e.getStackTrace()[i];
-		}
-		System.out.println(msg);
-		this.code = code;
-	}
-	
-	public String getMsg() {
-		return msg;
-	}
-	
-	public ServletManager.Code getCode() {
-		return code;
-	}
+    public GeneralException(ServletManager.Code code, String msg) {
+        this.msg = msg;
+        this.code = code;
+        this.code1 = null;
+        System.out.println(msg);
+    }
+
+    public GeneralException(ServletManager.Code code, Exception e) {
+        this.msg = e.toString() + ".\nStackTrace :";
+        for (int i = 0; i < e.getStackTrace().length; i++) {
+            this.msg += "\n" + e.getStackTrace()[i];
+        }
+        System.out.println(msg);
+        this.code = code;
+        this.code1 = null;
+    }
+
+    public GeneralException(com.Ease.API.Utils.ServletManager.Code code, String msg) {
+        this.msg = msg;
+        this.code1 = code;
+        this.code = null;
+        System.out.println(msg);
+    }
+
+    public GeneralException(com.Ease.API.Utils.ServletManager.Code code, Exception e) {
+        this.msg = e.toString() + ".\nStackTrace :";
+        for (int i = 0; i < e.getStackTrace().length; i++) {
+            this.msg += "\n" + e.getStackTrace()[i];
+        }
+        System.out.println(msg);
+        this.code1 = code;
+        this.code = null;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public ServletManager.Code getCode() {
+        return code;
+    }
+
+    public com.Ease.API.Utils.ServletManager.Code getCode1() {
+        return code1;
+    }
 }
