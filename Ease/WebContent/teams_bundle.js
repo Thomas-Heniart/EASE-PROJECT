27f3a886be0e767aed949e62f918525639c6c081
Fisun <sergii.fisun@epitech.eu>
2017-06-13 10:47:34 +0200

    teams_bundle

--- Ease/WebContent/teams_bundle.js
+++ Ease/WebContent/teams_bundle.js
@@ -19890,7 +19890,7 @@ function reducer() {
         for (var i = 0; i < nState.item.apps.length; i++) {
           if (nState.item.apps[i].id === action.payload.app_id) {
             app = nState.item.apps[i];
-            for (var j = 0; j < app.receivers.length; i++) {
+            for (var j = 0; j < app.receivers.length; j++) {
               if (app.receivers[j].team_user_id === action.payload.team_user_info) {
                 app.receivers.splice(j, 1);
                 return nState;
