bc0db1741858797681d83e1bca9efcb31c92a6e1
Thomas Heniart <heniart.thomas@gmail.com>
2017-06-14 09:49:30 +0200

    Remove useless code

--- Ease/src/com/Ease/Team/Channel.java
+++ Ease/src/com/Ease/Team/Channel.java
@@ -158,16 +158,4 @@ public class Channel {
             return;
         this.purpose = purpose;
     }
-
-    public void delete(DataBaseConnection db) {
-        try {
-            int transaction = db.startTransaction();
-            DatabaseRequest request = db.prepareRequest("DELETE FROM channelAndTeamUserMap WHERE channel_id = ?");
-            request.setInt(this.getDb_id());
-            request.set();
-            db.commitTransaction(transaction);
-        } catch (GeneralException e) {
-            e.printStackTrace();
-        }
-    }
 }
