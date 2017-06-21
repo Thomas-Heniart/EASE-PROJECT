2490199aa44554e419e6ae4c0cf89be221a9fa2c
Thomas Heniart <heniart.thomas@gmail.com>
2017-06-21 14:03:05 +0200

    Delete shareable app

--- Ease/src/com/Ease/API/V1/Teams/ServletDeleteShareableApp.java
+++ Ease/src/com/Ease/API/V1/Teams/ServletDeleteShareableApp.java
@@ -30,13 +30,11 @@ public class ServletDeleteShareableApp extends HttpServlet {
             sm.needToBeTeamUserOfTeam(team_id);
             TeamManager teamManager = (TeamManager) sm.getContextAttr("teamManager");
             Team team = teamManager.getTeamWithId(team_id);
-            TeamUser teamUser_connected = sm.getTeamUserForTeam(team);
-            Integer teamUser_id = sm.getIntParam("team_user_id", true);
-            TeamUser teamUser = team.getTeamUserWithId(teamUser_id);
-            if ((teamUser != teamUser_connected) && !teamUser_connected.isTeamAdmin())
-                throw new HttpServletException(HttpStatus.Forbidden, "You are not allowed to do this.");
             Integer shareableApp_id = sm.getIntParam("app_id", true);
             ShareableApp shareableApp = team.getAppManager().getShareableAppWithId(shareableApp_id);
+            TeamUser teamUser_connected = sm.getTeamUserForTeam(team);
+            if ((teamUser_connected != shareableApp.getTeamUser_owner()) && !teamUser_connected.isTeamAdmin())
+                throw new HttpServletException(HttpStatus.Forbidden, "You are not allowed to do this.");
             team.getAppManager().removeShareableApp(shareableApp, sm.getDB());
             sm.setSuccess("ShareableApp deleted");
         } catch (Exception e) {
