a6f255aa38314d434d48151fb103ccad7da8e7d7
Thomas Heniart <heniart.thomas@gmail.com>
2017-07-03 11:47:36 +0200

    GetMyInformation

---
+++ Ease/src/com/Ease/API/V1/Common/ServletGetMyInformation.java
@@ -0,0 +1,35 @@
+package com.Ease.API.V1.Common;
+
+import com.Ease.Dashboard.User.User;
+import com.Ease.Utils.Servlets.GetServletManager;
+import org.json.simple.JSONObject;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/api/v1/common/GetMyInformation")
+public class ServletGetMyInformation extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        GetServletManager sm = new GetServletManager(this.getClass().getName(), request, response, true);
+        try {
+            sm.needToBeConnected();
+            User user = sm.getUser();
+            JSONObject res = new JSONObject();
+            res.put("first_name", user.getFirstName());
+            sm.setSuccess(res);
+        } catch (Exception e) {
+            sm.setError(e);
+        }
+        sm.sendResponse();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+        rd.forward(request, response);
+    }
+}
