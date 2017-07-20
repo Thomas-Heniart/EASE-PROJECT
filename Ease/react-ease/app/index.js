a5dad9428b409ed36a176fd3eabd1b9180c34269
Fisun <sergii.fisun@epitech.eu>
2017-07-20 17:14:04 +0200

    index

--- Ease/react-ease/app/index.js
+++ Ease/react-ease/app/index.js
@@ -1,10 +1,32 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-import {Router, Route, IndexRoute, hashHistory} from 'react-router';
-import {Provider} from "react-redux"
+import {HashRouter, Layout, Route, IndexRoute, HashHistory} from 'react-router-dom';
+import {Provider} from "react-redux";
+import { CookiesProvider, withCookies, Cookies } from 'react-cookie';
 import store from "./store"
 var TeamView = require('./components/TeamView.js');
+var TeamCreationView = require('./components/teams/TeamCreationView');
+var SimpleTeamCreationView = require('./components/teams/SimpleTeamCreationView');
+var TeamJoinView = require('./components/teams/TeamJoinView');
+var Login = require('./components/common/Login');
+var Base = require('./components/common/Base.js');
 
-ReactDOM.render(<Provider store={store}>
-  <TeamView team_id="2"/>
-    </Provider>, document.getElementById('teamsHandler'));
+
+class App extends React.Component {
+  render(){
+    return (
+        <HashRouter>
+          <Base>
+            <Route exact path={"/"} component={TeamView}/>
+            <Route path="/teamCreation" component={TeamCreationView}/>
+            <Route path="/simpleTeamCreation" component={SimpleTeamCreationView}/>
+            <Route path="/teamJoin/:code" component={TeamJoinView}/>
+            <Route path="/teams/:teamId" component={TeamView}/>
+            <Route path="/login" component={Login}/>
+          </Base>
+        </HashRouter>
+    )
+  }
+}
+
+ReactDOM.render(<CookiesProvider><Provider store={store}><App/></Provider></CookiesProvider>, document.getElementById('app'));
