d4e8e2206ef981d7f27004fad7c8af345358aec0
Thomas Heniart <heniart.thomas@gmail.com>
2017-08-07 21:24:09 +0200

    Remove RequestListener and modify web xml

--- Ease/src/com/Ease/Utils/Servlets/PostServletManager.java
+++ Ease/src/com/Ease/Utils/Servlets/PostServletManager.java
@@ -3,10 +3,14 @@ package com.Ease.Utils.Servlets;
 import com.Ease.Utils.HttpServletException;
 import com.Ease.Utils.HttpStatus;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Date;
 
 public class PostServletManager extends ServletManager {
@@ -15,7 +19,21 @@ public class PostServletManager extends ServletManager {
 
     public PostServletManager(String servletName, HttpServletRequest request, HttpServletResponse response, boolean saveLogs) throws IOException {
         super(servletName, request, response, saveLogs);
-        params = (JSONObject) request.getAttribute("params");
+        BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream()));
+        String json = "";
+        String buffer;
+        while ((buffer = br.readLine()) != null)
+            json += buffer;
+        br.close();
+        if (!json.startsWith("{"))
+            return;
+        JSONParser jsonParser = new JSONParser();
+        try {
+            params = (JSONObject) jsonParser.parse(json);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            throw new IOException();
+        }
         Long now = this.getLongParam("now", false);
     }
 
