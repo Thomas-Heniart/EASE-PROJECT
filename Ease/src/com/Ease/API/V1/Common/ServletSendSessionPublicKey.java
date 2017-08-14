7e5e1f109517ae41ae4227743dcd85bff93a43f8
Thomas Heniart <heniart.thomas@gmail.com>
2017-08-14 13:40:45 +0200

    Commit send session public key to pull

---
+++ Ease/src/com/Ease/API/V1/Common/ServletSendSessionPublicKey.java
@@ -0,0 +1,34 @@
+package com.Ease.API.V1.Common;
+
+import com.Ease.Utils.Servlets.PostServletManager;
+import org.json.simple.JSONObject;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/SendSessionPublicKey")
+public class ServletSendSessionPublicKey extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PostServletManager sm = new PostServletManager(this.getClass().getName(), request, response, true);
+        try {
+            String sessionPublicKey = sm.getStringParam("publicKey", false);
+            sm.getSession().setAttribute("publicKey", sessionPublicKey);
+            JSONObject res = new JSONObject();
+            res.put("publicKey", sm.getContextAttr("publicKey"));
+            sm.setSuccess(res);
+        } catch (Exception e) {
+            sm.setError(e);
+        }
+        sm.sendResponse();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+        rd.forward(request, response);
+    }
+}
