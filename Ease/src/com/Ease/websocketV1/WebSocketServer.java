507a05a0590947e09511a1254eb2523ddbeb760d
Thomas Heniart <heniart.thomas@gmail.com>
2017-08-14 18:25:22 +0200

    Work on webSocket dispatching

--- Ease/src/com/Ease/websocketV1/WebSocketServer.java
+++ Ease/src/com/Ease/websocketV1/WebSocketServer.java
@@ -45,7 +45,7 @@ public class WebSocketServer {
                     channel.getWebSocketManager().addWebSocketSession(webSocketSession);
             }
             System.out.println("webSocketSession registered for user : " + user.getEmail());
-            webSocketSession.sendObject(new WebSocketMessage("CONNECTION_ID", session.getId()).toJSONObject());
+            session.getBasicRemote().sendText(new WebSocketMessage("CONNECTION_ID", session.getId()).toJSONObject().toString());
         }
     }
 
