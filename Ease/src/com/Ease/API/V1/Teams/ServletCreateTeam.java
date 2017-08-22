cde26957cb37901a532f7e9ad0996877cb3bb2b2
Thomas Heniart <heniart.thomas@gmail.com>
2017-08-22 13:59:16 +0200

    Create team error return

--- Ease/src/com/Ease/API/V1/Teams/ServletCreateTeam.java
+++ Ease/src/com/Ease/API/V1/Teams/ServletCreateTeam.java
@@ -95,8 +95,10 @@ public class ServletCreateTeam extends HttpServlet {
     private void checkUsernameIntegrity(String username) throws HttpServletException {
         if (username == null || username.equals(""))
             throw new HttpServletException(HttpStatus.BadRequest, "Usernames can't be empty!");
-        if (username.length() >= 22)
-            throw new HttpServletException(HttpStatus.BadRequest, "Sorry, that's a bit too long! Usernames must be fewer than 22 characters.");
+        if (username.length() < 4 || username.length() >= 22)
+            throw new HttpServletException(HttpStatus.BadRequest, "Sorry, usernames must be greater than 3 characters and fewer than 22 characters.");
+        /* if (username.length() >= 22)
+            throw new HttpServletException(HttpStatus.BadRequest, "Sorry, that's a bit too long! Usernames must be fewer than 22 characters."); */
         if (!username.equals(username.toLowerCase()))
             throw new HttpServletException(HttpStatus.BadRequest, "Sorry, usernames must be lowercase!");
         if (!Regex.isValidUsername(username))
