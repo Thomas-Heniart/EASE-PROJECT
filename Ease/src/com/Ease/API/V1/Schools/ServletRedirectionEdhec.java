cd1bcc3d9e9cbf5dd4d3d7c21d85ace9321b81fe
Thomas Heniart <heniart.thomas@gmail.com>
2017-08-29 11:03:12 +0200

    Fix redirection

--- Ease/src/com/Ease/API/V1/Schools/ServletRedirectionEdhec.java
+++ Ease/src/com/Ease/API/V1/Schools/ServletRedirectionEdhec.java
@@ -8,14 +8,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/isc-paris")
-public class ServletRedirectIscParis extends HttpServlet {
+@WebServlet("/edhec")
+public class ServletRedirectionEdhec extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         this.doGet(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        RequestDispatcher rd = request.getRequestDispatcher("schoolLandingTemplate.jsp?schoolImageSrcs=/resources/landing/school/iscparis.png&schoolName=ISC%20Paris&school=IscParis&commentFile=esticeComments.jsp&emailPlaceholder=iscparis.com&video=estice.mp4");
+        RequestDispatcher rd = request.getRequestDispatcher("schoolLandingTemplate.jsp?schoolImageSrcs=/resources/landing/school/edhec.png&schoolName=EDHEC&school=Edhec&commentFile=edhecComments.jsp&emailPlaceholder=edhec.com&video=edhec.mp4");
         rd.forward(request, response);
     }
 }
--- Ease/src/com/Ease/API/V1/Schools/ServletRedirectionEstice.java
+++ Ease/src/com/Ease/API/V1/Schools/ServletRedirectionEstice.java
@@ -9,24 +9,24 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * Servlet implementation class ServletRedirectEdhec
+ * Servlet implementation class ServletRedirectionEstice
  */
-@WebServlet("/edhec-school")
-public class ServletRedirectEdhec extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+@WebServlet({"/espas-estice-icm"})
+public class ServletRedirectionEstice extends HttpServlet {
+	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ServletRedirectEdhec() {
-        super();
+	public ServletRedirectionEstice() {
+		super();
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        RequestDispatcher rd = request.getRequestDispatcher("schoolLandingTemplate.jsp?schoolImageSrcs=/resources/landing/school/edhec.png&schoolName=EDHEC&school=Edhec&commentFile=edhecComments.jsp&emailPlaceholder=edhec.com&video=edhec.mp4");
+        RequestDispatcher rd = request.getRequestDispatcher("schoolLandingTemplate.jsp?schoolImageSrcs=/resources/landing/school/Espas.png,/resources/landing/school/Estice.png,/resources/landing/school/ICM.jpg&schoolName=ESPAS,%20ESTICE%20et%20ICM&school=Estice&commentFile=esticeComments.jsp&emailPlaceholder=something&video=estice.mp4");
         rd.forward(request, response);
 	}
 
