0b632d1e50b4e8c22ac69dd9f07f6958ea588a2d
Thomas Heniart <heniart.thomas@gmail.com>
2017-08-30 19:58:51 +0200

    Send digits again old landing

--- Ease/WebContent/js/popups/registrationPopup.js
+++ Ease/WebContent/js/popups/registrationPopup.js
@@ -9,6 +9,13 @@ var signUpPopup = function (elem) {
         self.qRoot.find("#3").addClass("show");
         self.qRoot.find("#1").removeClass("show");
         self.qRoot.find("#2").removeClass("show");
+        $("#3 #digits-email", self.qRoot).click(function (e) {
+            e.preventDefault();
+            ajaxHandler.post($("#1 form", self.qRoot).attr("action"),
+                {
+                    email: self.email,
+                });
+        });
         self.handler.addClass('myshow');
         $('body').css('overflow', 'hidden');
     }
