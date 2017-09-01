5a182adb5ccc03be4f15ae138c329156eb438e3a
Thomas Heniart <heniart.thomas@gmail.com>
2017-09-01 17:27:17 +0200

    backoffice website

--- Ease/WebContent/js/admin2.js
+++ Ease/WebContent/js/admin2.js
@@ -13,11 +13,11 @@ $(document).ready(function () {
 
 function addRow(website) {
     var elem = $('<tr>'
-        + '<td>' + website.id + '</td>'
-        + '<td>' + website.name + '</td>'
-        + '<td><img class="ui mini image" src="' + website.logo + '" /> </td>'
-        + '<td>' + website.login_url + '</td>'
-        + '<td>' + website.landing_url + '</td>'
+        + '<td class="id">' + website.id + '</td>'
+        + '<td class="name">' + website.name + '</td>'
+        + '<td class="logo"><img class="ui mini image" src="' + website.logo + '" /> </td>'
+        + '<td class="login_url">' + website.login_url + '</td>'
+        + '<td class="landing_url">' + website.landing_url + '</td>'
         + '<td> <div class="ui checkbox public"><input type="checkbox"/><label></label></div></td>'
         + '<td><i class="fa fa-pencil edit-website"/></td>'
         + '<td> <div class="ui checkbox delete"><input type="checkbox"/><label></label></div></td>'
@@ -36,12 +36,12 @@ function addRow(website) {
         });
     });
     $(".edit-website", elem).click(function () {
-        openWebsiteIntegration(website);
+        openWebsiteIntegration(website, elem);
     });
     return elem;
 }
 
-function openWebsiteIntegration(website) {
+function openWebsiteIntegration(website, websiteElem) {
     var modal = $("#website-integration");
     var edit_website = $("#website-edition", modal);
     var name = $("input[name='name']", edit_website);
@@ -58,6 +58,7 @@ function openWebsiteIntegration(website) {
         integrated.prop("checked", true);
     }
     edit_website.submit(function (e) {
+        e.stopPropagation();
         e.preventDefault();
         var action = $(this).attr("action");
         ajaxHandler.post(action, {
@@ -75,7 +76,15 @@ function openWebsiteIntegration(website) {
             website.login_url = login_url.val();
             website.folder = folder.val();
             website.integrated = integrated.is(":checked");
+            if (!website.integrated)
+                websiteElem.addClass(negative);
+            $(".name", websiteElem).text(website.name);
+            $(".landing_url", websiteElem).text(website.landing_url);
+            $(".login_url", websiteElem).text(website.login_url);
+            $(".logo img", websiteElem).attr("src", "/resources/websites/" + website.folder + "/logo.png");
         });
+        edit_website.off("submit");
+        modal.modal("hide");
     });
     modal.modal("show");
 }
