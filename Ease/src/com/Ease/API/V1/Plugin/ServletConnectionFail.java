1c5493a558768647e6d4b1d186aebd7850b6ffc8
Thomas Heniart <heniart.thomas@gmail.com>
2017-09-12 10:10:59 +0200

    Some fix

---
+++ Ease/src/com/Ease/API/V1/Plugin/ServletConnectionFail.java
@@ -0,0 +1,31 @@
+package com.Ease.API.V1.Plugin;
+
+import com.Ease.Utils.Servlets.PostServletManager;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/api/v1/plugin/ConnectionFail")
+public class ServletConnectionFail extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PostServletManager sm = new PostServletManager(this.getClass().getName(), request, response, true);
+        try {
+            String website = sm.getStringParam("website", true);
+            System.out.println(website);
+            sm.setSuccess("Email sent");
+        } catch (Exception e) {
+            sm.setError(e);
+        }
+        sm.sendResponse();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+        rd.forward(request, response);
+    }
+}
