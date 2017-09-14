9030b399ae433b9c63ece5635428d72d7996b5f3
Thomas Heniart <heniart.thomas@gmail.com>
2017-09-14 02:07:03 +0200

    Safari extension

--- Ease/src/com/Ease/Context/Variables.java
+++ Ease/src/com/Ease/Context/Variables.java
@@ -2,17 +2,18 @@ package com.Ease.Context;
 
 public class Variables {
 
-    public static final String stripe_test_api_key = "sk_test_4Qqw6xcv7VQDmXBS5CZ9rz5T";
     public static final String MJ_APIKEY_PUBLIC = "c6b4097b55dc8d93c46f44c621efeca6";
     public static final String MJ_APIKEY_PRIVATE = "c3e6e3b927fdbefd4b3f5d2b25a31ce2";
 
     //Prod
+    /*
     public static final String ENVIRONNEMENT = "Prod";
 	public static final String PROJECT_PATH = "/usr/share/tomcat8/webapps/ROOT";
 	public static final String KEYS_PATH = "/usr/share/tomcat8/webapps/ROOT/WEB-INF/classes/com/Ease/Utils/Crypto/privateKeys.txt";
 	public static final String WEBSITES_PATH = "/resources/websites/";
 	public static final String URL_PATH = "https://ease.space/";
 	public static final String DATABASE_IP = "127.0.0.1";
+	*/
     //Prod
     /*
     public static final String ENVIRONNEMENT = "Prod";
@@ -50,12 +51,10 @@ public class Variables {
     public static final String URL_PATH = "http://localhost:8080/";
     */
     //Toto
-    /*
     public static final String ENVIRONNEMENT = "PC de Toto";
     public static final String PROJECT_PATH = "/Users/thomas/EASE-PROJECT/Ease/WebContent";
     public static final String KEYS_PATH = "/Users/thomas/EASE-PROJECT/Ease/src/com/Ease/Utils/Crypto/privateKeys.txt";
     public static final String WEBSITES_PATH = "/resources/websites/";
     public static final String URL_PATH = "https://localhost:8443/";
     public static final String DATABASE_IP = "127.0.0.1";
-    */
 }
