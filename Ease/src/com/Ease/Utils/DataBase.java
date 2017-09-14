699a4c2fb5b13f1a885fbe9fe3085ae09b7c2f09
Thomas Heniart <heniart.thomas@gmail.com>
2017-09-14 12:44:52 +0200

    Contact page + use of concurrent hashmap

--- Ease/src/com/Ease/Utils/DataBase.java
+++ Ease/src/com/Ease/Utils/DataBase.java
@@ -11,6 +11,14 @@ public final class DataBase {
 	private static final BasicDataSource dataSource = new BasicDataSource();
 
     static {
+        dataSource.setDefaultAutoCommit(true);
+        dataSource.setInitialSize(10);
+        dataSource.setMaxIdle(100);
+        dataSource.setMinIdle(10);
+        dataSource.setMaxWaitMillis(30000);
+        dataSource.setTimeBetweenEvictionRunsMillis(5000);
+        dataSource.setMinEvictableIdleTimeMillis(60000);
+        dataSource.setPoolPreparedStatements(true);
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://" + Variables.DATABASE_IP + ":3306/ease?charset=utf8");
         dataSource.setUsername("client");
