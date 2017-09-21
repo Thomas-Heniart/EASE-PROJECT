20678e4776e3555621aa83b2a234d017c964f22a
Thomas Heniart <heniart.thomas@gmail.com>
2017-09-21 18:11:09 +0200

    Edit create single app

--- Ease/src/com/Ease/Dashboard/App/WebsiteApp/ClassicApp/Account.java
+++ Ease/src/com/Ease/Dashboard/App/WebsiteApp/ClassicApp/Account.java
@@ -92,7 +92,7 @@ public class Account {
         return account;
     }
 
-    public static Account createShareableAccount(List<JSONObject> accountInformationObjList, String deciphered_teamKey, Integer reminderValue, DataBaseConnection db) throws GeneralException {
+    public static Account createShareableAccount(JSONObject account_information, String deciphered_teamKey, Integer reminderValue, DataBaseConnection db) throws GeneralException {
         Map.Entry<String, String> publicAndPrivateKey = RSA.generateKeys();
         String publicKey = publicAndPrivateKey.getKey();
         String privateKey = publicAndPrivateKey.getValue();
@@ -109,7 +109,7 @@ public class Account {
         request.setString(publicKey);
         request.setString(ciphered_key);
         String db_id = request.set().toString();
-        List<AccountInformation> accountInformationList = AccountInformation.createAccountInformations(db_id, accountInformationObjList, publicKey, db);
+        List<AccountInformation> accountInformationList = AccountInformation.createAccountInformations(db_id, account_information, publicKey, db);
         db.commitTransaction(transaction);
         Account account = new Account(db_id, false, publicKey, ciphered_key, accountInformationList, (reminderValue == null ? 0 : reminderValue));
         account.setPrivateKey(privateKey);
