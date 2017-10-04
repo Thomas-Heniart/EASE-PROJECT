76ce924d4f41940848f8a366bbb3ba85cc9ac993
NiceTwice <fisun.serge76@gmail.com>
2017-10-04 16:31:29 +0200

    room manager can see information

--- Ease/react-ease/app/components/teams/TeamsPreview.js
+++ Ease/react-ease/app/components/teams/TeamsPreview.js
@@ -3,16 +3,16 @@ import {NavLink} from 'react-router-dom';
 import {Icon, Popup} from 'semantic-ui-react';
 
 const TeamPlanPopup = ({text, trigger}) => {
-    return (
-        <Popup size="mini"
-               position="top center"
-               inverted
-               id="team_plan_popup"
-               trigger={
-                   trigger
-               }
-               content={text}/>
-    )
+  return (
+      <Popup size="mini"
+             position="top center"
+             inverted
+             id="team_plan_popup"
+             trigger={
+               trigger
+             }
+             content={text}/>
+  )
 };
 
 function Step1(props){
@@ -36,69 +36,69 @@ function Step1(props){
 }
 
 function TeamBasicPlan(props) {
-    return (
-        <div class="team_plan" id="starter_plan" style={{marginRight: '40px'}}>
-            <h1 class="text-center title">Basic</h1>
-            <span class="text-center price">0 <span class="symbol">€</span></span>
-            <span class="price_divider">per month, per active user</span>
-            <div class="text-center" style={{marginBottom: '35px'}}>
-                <button class="button-unstyle big-button button">
-                    <NavLink to={`/main/simpleTeamCreation?plan_id=0`} className="link-unstyle"
-                             activeClassName="active">
-                        CREATE A FREE TEAM
-                    </NavLink>
-                </button>
-            </div>
-            <span class="info">INCLUDED IN THE <TeamPlanPopup text={'Collaborative interface to handle passwords accessible by team members'}
-                                                              trigger={<span
-                                                                  class="text-underlined">TEAM SPACE</span>}/> <TeamPlanPopup
-                text={'Collaborative interface to handle passwords accessible by team members'}
-                trigger={<Icon link name="help circle outline"/>}/></span>
-            <div class="display-flex flex_direction_column full_flex">
-                <div class="feature">
-                    <i class="fa fa-check tic"/>
-                    <span>Up to 30 team members</span>
-                </div>
-                <div class="feature">
-                    <i class="fa fa-check tic"/>
-                    <span>One to one & group password sharing</span>
-                </div>
-                <div class="feature">
-                    <i class="fa fa-check tic"/>
-                    <span>Unlimited team passwords</span>
-                </div>
-                <div class="feature">
-                    <i class="fa fa-check tic"/>
-                    <span>Up to 4 <TeamPlanPopup
-                        text={'Rooms group the people  who work together so sharing & organizing passwords is easier'}
-                        trigger={<span class="text-underlined">rooms</span>}/></span>
-                </div>
-                <div class="feature">
-                  <i class="fa fa-check tic"/>
-                  <span>Password modification policy</span>
-                </div>
-                <div class="feature">
-                    <i class="fa fa-check tic"/>
-                    <span>1 <TeamPlanPopup text={'Admins can activate, deactivate members, give them access to tools…'}
-                                           trigger={<span class="text-underlined">Admin</span>}/></span>
-                </div>
-                <div class="feature">
-                    <i class="fa fa-check tic"/>
-                    <span><TeamPlanPopup
-                        text={'Notifications makes it all work smoothly, it simplifies interacting with team members'}
-                        trigger={<span class="text-underlined">Notifications</span>}/></span>
-                </div>
-            </div>
-            <div class="text-center" style={{margin: '60px 0 0 0'}}>
-                <button class="button-unstyle big-button button">
-                    <NavLink to={`/main/simpleTeamCreation?plan_id=0`} className="link-unstyle"
-                             activeClassName="active">
-                        CREATE A FREE TEAM
-                    </NavLink>
-                </button>
-            </div>
+  return (
+      <div class="team_plan" id="starter_plan" style={{marginRight: '40px'}}>
+        <h1 class="text-center title">Basic</h1>
+        <span class="text-center price">0 <span class="symbol">€</span></span>
+        <span class="price_divider">per month, per active user</span>
+        <div class="text-center" style={{marginBottom: '35px'}}>
+          <button class="button-unstyle big-button button">
+            <NavLink to={`/main/simpleTeamCreation?plan_id=0`} className="link-unstyle"
+                     activeClassName="active">
+              CREATE A FREE TEAM
+            </NavLink>
+          </button>
         </div>
-    )
+        <span class="info">INCLUDED IN THE <TeamPlanPopup text={'Collaborative interface to handle passwords accessible by team members'}
+                                                          trigger={<span
+                                                              class="text-underlined">TEAM SPACE</span>}/> <TeamPlanPopup
+            text={'Collaborative interface to handle passwords accessible by team members'}
+            trigger={<Icon link name="help circle outline"/>}/></span>
+        <div class="display-flex flex_direction_column full_flex">
+          <div class="feature">
+            <i class="fa fa-check tic"/>
+            <span>Up to 30 team members</span>
+          </div>
+          <div class="feature">
+            <i class="fa fa-check tic"/>
+            <span>One to one & group password sharing</span>
+          </div>
+          <div class="feature">
+            <i class="fa fa-check tic"/>
+            <span>Unlimited team passwords</span>
+          </div>
+          <div class="feature">
+            <i class="fa fa-check tic"/>
+            <span>Up to 4 <TeamPlanPopup
+                text={'Rooms group the people  who work together so sharing & organizing passwords is easier'}
+                trigger={<span class="text-underlined">rooms</span>}/></span>
+          </div>
+          <div class="feature">
+            <i class="fa fa-check tic"/>
+            <span>Password modification policy</span>
+          </div>
+          <div class="feature">
+            <i class="fa fa-check tic"/>
+            <span>1 <TeamPlanPopup text={'Admins can activate, deactivate members, give them access to tools…'}
+                                   trigger={<span class="text-underlined">Admin</span>}/></span>
+          </div>
+          <div class="feature">
+            <i class="fa fa-check tic"/>
+            <span><TeamPlanPopup
+                text={'Notifications makes it all work smoothly, it simplifies interacting with team members'}
+                trigger={<span class="text-underlined">Notifications</span>}/></span>
+          </div>
+        </div>
+        <div class="text-center" style={{margin: '60px 0 0 0'}}>
+          <button class="button-unstyle big-button button">
+            <NavLink to={`/main/simpleTeamCreation?plan_id=0`} className="link-unstyle"
+                     activeClassName="active">
+              CREATE A FREE TEAM
+            </NavLink>
+          </button>
+        </div>
+      </div>
+  )
 }
 
 function TeamProPlan(props){
@@ -106,72 +106,72 @@ function TeamProPlan(props){
       <div class="team_plan" id="pro_team_plan" style={{marginRight: '40px'}}>
         <img src="/resources/other/illu.svg" alt="icon" class="styleImage"/>
         <h1 class="text-center title">Pro</h1>
-          <span class="text-center price">3,99 <span class="symbol">€HT</span></span>
-          <span class="price_divider">per month, per active user</span>
+        <span class="text-center price">3,99 <span class="symbol">€HT</span></span>
+        <span class="price_divider">per month, per active user</span>
         <div class="text-center">
           <button class="button-unstyle big-button button">
-              <NavLink to={`/main/simpleTeamCreation?plan_id=1`} className="link-unstyle" activeClassName="active">
-                  TRY 1 MONTH FREE
+            <NavLink to={`/main/simpleTeamCreation?plan_id=1`} className="link-unstyle" activeClassName="active">
+              TRY 1 MONTH FREE
             </NavLink>
           </button>
         </div>
-          <span class="tip" style={{margin: '5px 0 13px 0'}}>No credit card required</span>
-          <span class="info">ALL FEATURES FROM STARTER PLAN, AND:</span>
+        <span class="tip" style={{margin: '5px 0 13px 0'}}>No credit card required</span>
+        <span class="info">ALL FEATURES FROM STARTER PLAN, AND:</span>
         <div class="display-flex flex_direction_column full_flex">
           <div class="feature">
             <i class="fa fa-check tic"/>
-              <span>Unlimited <TeamPlanPopup
-                  text={'Rooms group the people  who work together so sharing & organizing passwords is easier'}
-                  trigger={<span class="text-underlined">rooms</span>}/></span>
+            <span>Unlimited <TeamPlanPopup
+                text={'Rooms group the people  who work together so sharing & organizing passwords is easier'}
+                trigger={<span class="text-underlined">rooms</span>}/></span>
           </div>
           <div class="feature">
             <i class="fa fa-check tic"/>
-              <span>Unlimited team members</span>
+            <span>Unlimited team members</span>
           </div>
           <div class="feature">
             <i class="fa fa-check tic"/>
-              <span>Unlimited admins and 1 owner</span>
+            <span>Unlimited admins and 1 owner</span>
           </div>
           <div class="feature">
             <i class="fa fa-check tic"/>
-              <span><TeamPlanPopup
-                  text={'Room managers are responsible to administer people and apps accesses within a room'}
-                  trigger={<span class="text-underlined">Room Managers</span>}/> set up</span>
+            <span><TeamPlanPopup
+                text={'Room managers are responsible to administer people and apps accesses within a room'}
+                trigger={<span class="text-underlined">Room Managers</span>}/> set up</span>
           </div>
           <div class="feature">
             <i class="fa fa-check tic"/>
-              <span>Automatic <TeamPlanPopup
-                  text={'You can set up departure dates on members to remove accesses automatically'}
-                  trigger={<span class="text-underlined">access removal</span>}/></span>
+            <span>Automatic <TeamPlanPopup
+                text={'You can set up departure dates on members to remove accesses automatically'}
+                trigger={<span class="text-underlined">access removal</span>}/></span>
           </div>
           <div class="feature">
             <i class="fa fa-check tic"/>
-              <span>Priority customer support</span>
+            <span>Priority customer support</span>
           </div>
           <div class="feature align_items_center">
             <i class="fa fa-check tic"/>
-              <span class="full_flex">Temporary password sharing</span>
+            <span class="full_flex">Temporary password sharing</span>
             <img src="/resources/icons/Soon.png" style={{width: '56px', height: '20px'}}/>
           </div>
           <div class="feature align_items_center">
             <i class="fa fa-check tic"/>
-              <span class="full_flex">Software credential sharing</span>
+            <span class="full_flex">Software credential sharing</span>
             <img src="/resources/icons/Soon.png" style={{width: '56px', height: '20px'}}/>
           </div>
-            <div class="feature align_items_center">
-                <i class="fa fa-check tic"/>
-                <span class="full_flex">2FA login</span>
-              <img src="/resources/icons/Soon.png" style={{width: '56px', height: '20px'}}/>
+          <div class="feature align_items_center">
+            <i class="fa fa-check tic"/>
+            <span class="full_flex">2FA login</span>
+            <img src="/resources/icons/Soon.png" style={{width: '56px', height: '20px'}}/>
           </div>
         </div>
         <div class="text-center" style={{margin: '60px 0 10px 0'}}>
           <button class="button-unstyle big-button button">
-              <NavLink to={`/main/simpleTeamCreation?plan_id=1`} className="link-unstyle" activeClassName="active">
-                  TRY 1 MONTH FREE
+            <NavLink to={`/main/simpleTeamCreation?plan_id=1`} className="link-unstyle" activeClassName="active">
+              TRY 1 MONTH FREE
             </NavLink>
           </button>
         </div>
-          <span class="tip">No credit card required</span>
+        <span class="tip">No credit card required</span>
       </div>
   )
 }
@@ -181,42 +181,42 @@ function TeamEnterprisePlan(props){
       <div class="team_plan" id="enterprise_team_plan">
         <img src="/resources/other/Saturn.svg" alt="icon" class="styleImage"/>
         <div class="plan_header display-flex flex_direction_column text-center">
-            <h1 class="text-center title" style={{marginBottom: '25px'}}>Premium</h1>
-            <span class="text-center price">7,89 <span class="symbol">€HT</span></span>
-            <span style={{fontSize: '0.8rem', marginTop: '10px'}}>per month, per active user</span>
+          <h1 class="text-center title" style={{marginBottom: '25px'}}>Premium</h1>
+          <span class="text-center price">7,89 <span class="symbol">€HT</span></span>
+          <span style={{fontSize: '0.8rem', marginTop: '10px'}}>per month, per active user</span>
         </div>
-          <span class="info">ALL FEATURES FROM STARTER, PRO, AND:</span>
+        <span class="info">ALL FEATURES FROM STARTER, PRO, AND:</span>
         <div class="display-flex flex_direction_column full_flex">
           <div class="feature">
             <i class="fa fa-check tic"/>
-              <span><TeamPlanPopup text={'Companies using an internal SSO can integrate Ease.space'} trigger={<span
-                  class="text-underlined">Single Sign-on</span>}/> integration (SSO)</span>
+            <span><TeamPlanPopup text={'Companies using an internal SSO can integrate Ease.space'} trigger={<span
+                class="text-underlined">Single Sign-on</span>}/> integration (SSO)</span>
           </div>
           <div class="feature">
             <i class="fa fa-check tic"/>
-              <span>In house database</span>
+            <span>In house database</span>
           </div>
           <div class="feature">
             <i class="fa fa-check tic"/>
-              <span>Active Directory synchronisation</span>
+            <span>Active Directory synchronisation</span>
           </div>
           <div class="feature">
             <i class="fa fa-check tic"/>
-              <span>Activity logs</span>
+            <span>Activity logs</span>
           </div>
           <div class="feature">
             <i class="fa fa-check tic"/>
-              <span>Archives</span>
+            <span>Archives</span>
           </div>
           <div class="feature">
             <i class="fa fa-check tic"/>
-              <span>99.8% SLA</span>
+            <span>99.8% SLA</span>
           </div>
         </div>
         <div class="text-center">
           <button class="button-unstyle big-button button">
             <a href="/companyContact" class="link-unstyle">
-                CONTACT US
+              CONTACT US
             </a>
           </button>
         </div>
@@ -227,15 +227,15 @@ function TeamEnterprisePlan(props){
 function Step2(props){
   return (
       <div class="content display-flex flex_direction_column marginAuto step2">
-          <h1 class="text-center" style={{margin: '0 0 10px 0'}}>Fair and transparent pricing</h1>
+        <h1 class="text-center" style={{margin: '0 0 10px 0'}}>Fair and transparent pricing</h1>
         <span class="sub-title"><u>Monthly billing</u> per <u>active user</u>*, <u>cancel any time</u></span>
         <div class="display-flex" style={{margin: '55px 0 37px 0'}}>
-            <TeamBasicPlan/>
+          <TeamBasicPlan/>
           <TeamProPlan/>
           <TeamEnterprisePlan/>
         </div>
-          <span
-              class="sub-title">* a user is active from the day he/she shares or receives an app in his/her Team Space</span>
+        <span
+            class="sub-title">* a user is active from the day he/she shares or receives an app in his/her Team Space</span>
       </div>
   )
 }
