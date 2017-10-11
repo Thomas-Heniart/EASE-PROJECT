1d3019f07219efacf4f4a5976b2641f67d2a720e
Thomas Heniart <heniart.thomas@gmail.com>
2017-10-11 15:47:44 +0200

    Get personal and team space

--- Ease/src/com/Ease/API/Rest/GetPersonalAndTeamSpace.java
+++ Ease/src/com/Ease/API/Rest/GetPersonalAndTeamSpace.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/GetPersonalAndTeamSpace")
+@WebServlet("/api/rest/GetPersonalAndTeamSpace")
 public class GetPersonalAndTeamSpace extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         GetServletManager sm = new GetServletManager(this.getClass().getName(), request, response, true);
