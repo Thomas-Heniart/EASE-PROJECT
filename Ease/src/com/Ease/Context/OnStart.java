25acc2c54ea5d78c56ad851afa37686907ca03fb
Thomas Heniart <heniart.thomas@gmail.com>
2017-10-18 11:19:35 +0200

    Cleaner task

--- Ease/src/com/Ease/Context/OnStart.java
+++ Ease/src/com/Ease/Context/OnStart.java
@@ -67,11 +67,13 @@ public class OnStart implements ServletContextListener {
                 /* Timers */
                 Timer time = new Timer(); // Instantiate Timer Object
                 StripeScheduledTask st = new StripeScheduledTask(teamManager); // Instantiate SheduledTask class
-                time.schedule(st, 0, 12 * 60 * 60 * 1000); // Create Repetitively task for every 12 hours */
+                time.schedule(st, 12 * 60 * 60 * 1000, 12 * 60 * 60 * 1000); // Create Repetitively task for every 12 hours */
                 WebsiteScheduledTask websiteScheduledTask = new WebsiteScheduledTask(catalog);
                 time.schedule(websiteScheduledTask, 0, 24 * 60 * 60 * 1000);
                 RemindersScheduledTask reminders = new RemindersScheduledTask(teamManager);
-                time.schedule(reminders, 0, 24 * 60 * 60 * 1000);
+                time.schedule(reminders, 24 * 60 * 60 * 1000, 24 * 60 * 60 * 1000);
+                CleanerTask cleanerTask = new CleanerTask();
+                time.schedule(cleanerTask, 0, 24 * 60 * 60 * 1000);
                 List<String> colors = new ArrayList<String>();
                 colors.add("#373B60");
                 colors.add("#9B59B6");
