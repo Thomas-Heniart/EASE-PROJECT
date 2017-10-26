2cf25290ace3c5a3e43036798fd6cc031d391325
Thomas Heniart <heniart.thomas@gmail.com>
2017-10-26 14:37:02 +0200

    Some fixes

--- Ease/react-ease/app/components/teamAppAdders/EnterpriseTeamApp.js
+++ Ease/react-ease/app/components/teamAppAdders/EnterpriseTeamApp.js
@@ -459,7 +459,7 @@ class EnterpriseTeamApp extends Component {
                                  me={me}
                                  team_id={this.props.team_id}/>}
                 <div>
-                    {!this.state.edit && users.length > 2 &&
+                    {!this.state.edit && users.length > 3 &&
                     <ButtonShowMore number_of_users={users.length - 3} show_more={this.state.show_more} showMore={this.setShowMore}/>}
                 </div>
                 {this.state.edit &&
