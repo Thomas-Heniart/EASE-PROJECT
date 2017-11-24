53b3627272e6bc3fb5fc17f4ec43f1d417108217
Thomas Heniart <heniart.thomas@gmail.com>
2017-11-24 15:50:23 +0100

    Edit app name

--- Ease/src/com/Ease/API/V1/Dashboard/ServletEditAppName.java
+++ Ease/src/com/Ease/API/V1/Dashboard/ServletEditAppName.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/api/v1/teams/EditAppName")
+@WebServlet("/api/v1/dashboard/EditAppName")
 public class ServletEditAppName extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PostServletManager sm = new PostServletManager(this.getClass().getName(), request, response, true);
