8525ec7f48077678eb899f0b5fe028c47b80fb66
Thomas Heniart <heniart.thomas@gmail.com>
2017-11-24 16:38:58 +0100

    backoffice admin edit role

--- Ease/src/com/Ease/API/V1/Admin/ServletEditTeamUserRole.java
+++ Ease/src/com/Ease/API/V1/Admin/ServletEditTeamUserRole.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/ServletEditTeamUserRole")
+@WebServlet("/api/v1/admin/EditTeamUserRole")
 public class ServletEditTeamUserRole extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PostServletManager sm = new PostServletManager(this.getClass().getName(), request, response, true);
