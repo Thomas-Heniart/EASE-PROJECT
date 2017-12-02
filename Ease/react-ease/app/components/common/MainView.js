bf97875118b593a1c8c4ddeaf258b827a3628baf
NiceTwice <fisun.serge76@gmail.com>
2017-12-02 17:24:17 +0100

    dashboard

--- Ease/react-ease/app/components/common/MainView.js
+++ Ease/react-ease/app/components/common/MainView.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import EaseHeader from './EaseHeader';
 import TeamsPreview from '../teams/TeamsPreview';
 import Catalog from '../catalog/Catalog';
@@ -10,20 +10,22 @@ import { DragDropContext } from 'react-dnd';
 import CustomDragLayer from "../dashboard/CustomDragLayer";
 var SimpleTeamCreationView = require('../teams/SimpleTeamCreationView');
 
-function MainView(props){
-  return (
-      <div class="display-flex flex_direction_column full_flex" style={{flexFlow: 'column-reverse'}}>
-        <Switch>
-          <Route path='/main/teamsPreview' component={TeamsPreview}/>
-          <Route path='/main/simpleTeamCreation' component={SimpleTeamCreationView}/>
-          <Route path='/main/catalog' component={Catalog} />
-          <Route path='/main/settings' component={Settings} />
-          <Route path='/main/dashboard' component={Dashboard}/>
-        </Switch>
-        <EaseHeader/>
-        <CustomDragLayer/>
-      </div>
-  )
+class MainView extends Component {
+  render() {
+    return (
+        <div class="display-flex flex_direction_column full_flex" style={{flexFlow: 'column-reverse'}}>
+          <Switch>
+            <Route path='/main/teamsPreview' component={TeamsPreview}/>
+            <Route path='/main/simpleTeamCreation' component={SimpleTeamCreationView}/>
+            <Route path='/main/catalog' component={Catalog}/>
+            <Route path='/main/settings' component={Settings}/>
+            <Route path='/main/dashboard' component={Dashboard}/>
+          </Switch>
+          <EaseHeader/>
+          <CustomDragLayer/>
+        </div>
+    )
+  }
 }
 
 module.exports = DragDropContext(HTML5Backend)(MainView);
