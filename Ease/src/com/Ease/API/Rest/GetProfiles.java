af1e3f17755305ed377a46f60f4e9ec16548d5a8
Thomas Heniart <heniart.thomas@gmail.com>
2017-12-06 16:58:05 +0100

    Servlet mobile

--- Ease/src/com/Ease/API/Rest/GetProfiles.java
+++ Ease/src/com/Ease/API/Rest/GetProfiles.java
@@ -3,6 +3,8 @@ package com.Ease.API.Rest;
 import com.Ease.NewDashboard.App;
 import com.Ease.NewDashboard.Profile;
 import com.Ease.Team.Team;
+import com.Ease.Team.TeamCardReceiver.TeamCardReceiver;
+import com.Ease.Team.TeamCardReceiver.TeamSingleCardReceiver;
 import com.Ease.User.User;
 import com.Ease.Utils.Servlets.GetServletManager;
 import org.json.simple.JSONArray;
@@ -24,16 +26,20 @@ public class GetProfiles extends HttpServlet {
             sm.needToBeConnected();
             User user = sm.getUser();
             JSONObject profiles = new JSONObject();
+            JSONObject apps = new JSONObject();
             for (Profile profile : user.getProfileSet()) {
                 JSONObject jsonObject = new JSONObject();
                 jsonObject.put("name", profile.getProfileInformation().getName());
-                JSONArray apps = new JSONArray();
+                JSONArray app_ids = new JSONArray();
                 for (App app : profile.getAppSet()) {
                     if (!app.isEmpty() && !app.isLogWithApp()) {
                         if (app.isClassicApp()) {
                             String symmetric_key = null;
                             String team_key = null;
                             if (app.getTeamCardReceiver() != null) {
+                                TeamCardReceiver teamCardReceiver = app.getTeamCardReceiver();
+                                if (teamCardReceiver.getTeamCard().isTeamSingleCard() && !teamCardReceiver.getTeamUser().isTeamAdmin() && !((TeamSingleCardReceiver)teamCardReceiver).isAllowed_to_see_password())
+                                    continue;
                                 Team team = app.getTeamCardReceiver().getTeamCard().getTeam();
                                 if (!sm.getTeamUser(team).isDisabled())
                                     team_key = (String) sm.getTeamProperties(team.getDb_id()).get("teamKey");
@@ -41,14 +47,16 @@ public class GetProfiles extends HttpServlet {
                                 symmetric_key = (String) sm.getUserProperties(user.getDb_id()).get("keyUser");
                             app.decipher(symmetric_key, team_key);
                         }
+                        apps.put(app.getDb_id(), app.getRestJson());
+                        app_ids.add(app.getDb_id());
                     }
-                    apps.add(app.getRestJson());
                 }
-                jsonObject.put("apps", apps);
+                jsonObject.put("app_ids", app_ids);
                 profiles.put(profile.getDb_id(), jsonObject);
             }
             JSONObject res = new JSONObject();
             res.put("profiles", profiles);
+            res.put("apps", apps);
             sm.setSuccess(res);
         } catch (Exception e) {
             sm.setError(e);
