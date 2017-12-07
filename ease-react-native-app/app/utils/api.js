89494ed18e9fa9a1d77caaf9e51de36d04ef5bb1
NiceTwice <fisun.serge76@gmail.com>
2017-12-07 10:48:54 +0100

    dashboard

--- ease-react-native-app/app/utils/api.js
+++ ease-react-native-app/app/utils/api.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 
-//axios.defaults.baseURL = 'https://ease.space/api/rest';
-axios.defaults.baseURL = 'https://192.162.0.19:8443/api/rest';
+//axios.defaults.baseURL = 'https://ease.space/';
+axios.defaults.baseURL = 'http://192.168.0.19:8080/';
 
 const basic_get = (url, params) => {
   return axios.get(url, {params: params})
       .then(response => {
+        console.log(response.data);
         return response.data;
       })
       .catch(err => {
+        console.log(err.response.data);
         throw err.response.data;
       });
 };
@@ -28,25 +30,30 @@ const basic_post = (url, params) => {
 export default api = {
   post: {
     connection: ({email,password}) => {
-      return basic_post('/Connection', {
+      return basic_post('/api/rest/Connection', {
         email:email,
-        password:password,
-        timestamp: new Date().getTime()
+        password:password
       });
     }
   },
   get: {
+    fetchMyInformation: () => {
+      return basic_get('/api/v1/common/GetMyInformation');
+    },
+    fetchPersonalSpace: () => {
+      return basic_get('/api/rest/GetProfiles');
+    },
     getPersonalAndTeamSpace: () => {
-      return basic_get('/GetPersonalAndTeamSpace');
+      return basic_get('/api/rest/GetPersonalAndTeamSpace');
     },
     getTeamRoomApps: ({team_id, room_id}) => {
-      return basic_get('/GetRoomApps', {
+      return basic_get('/api/rest/GetRoomApps', {
         team_id: team_id,
         room_id: room_id
       });
     },
     getGroupApps: ({group_id}) => {
-      return basic_get('/GetGroupApps', {
+      return basic_get('/api/rest/GetGroupApps', {
         group_id: group_id
       });
     }
