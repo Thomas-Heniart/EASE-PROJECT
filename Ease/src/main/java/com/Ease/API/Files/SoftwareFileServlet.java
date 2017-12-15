cab3bbf6ca7bb4ae31310384262db7359cf0118d
Thomas Heniart <heniart.thomas@gmail.com>
2017-12-15 12:23:51 +0100

    Software file servlet

---
+++ Ease/src/main/java/com/Ease/API/Files/SoftwareFileServlet.java
@@ -0,0 +1,40 @@
+package com.Ease.API.Files;
+
+import com.Ease.Context.Variables;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+
+@WebServlet("/resources/software/*")
+public class SoftwareFileServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String filename = URLDecoder.decode(request.getPathInfo().substring(1), "UTF-8");
+        File file = new File(Variables.WEBSITES_FOLDER_PATH, filename);
+        response.setHeader("Content-Type", getServletContext().getMimeType(filename));
+        response.setHeader("Content-Length", String.valueOf(file.length()));
+        response.setHeader("Content-Disposition", "inline; filename=\"" + file.getName() + "\"");
+        try {
+            Files.copy(file.toPath(), response.getOutputStream());
+        } catch (NoSuchFileException e) {
+            file = new File(Variables.WEBSITES_FOLDER_PATH, "undefined/logo.png");
+            response.setHeader("Content-Length", String.valueOf(file.length()));
+            response.setHeader("Content-Disposition", "inline; filename=\"" + file.getName() + "\"");
+            Files.copy(file.toPath(), response.getOutputStream());
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+        rd.forward(request, response);
+    }
+}
