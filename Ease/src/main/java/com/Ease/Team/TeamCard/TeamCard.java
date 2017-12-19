699997a0dbbec486889c96148d7ee457e28292ad
Thomas Heniart <heniart.thomas@gmail.com>
2017-12-19 20:05:23 +0100

    Fix _ in sub_type

--- Ease/src/main/java/com/Ease/Team/TeamCard/TeamCard.java
+++ Ease/src/main/java/com/Ease/Team/TeamCard/TeamCard.java
@@ -139,7 +139,7 @@ abstract public class TeamCard {
         res.put("name", this.getName());
         res.put("logo", this.getLogo());
         res.put("type", this.getType());
-        res.put("subtype", this.getSubtype());
+        res.put("sub_type", this.getSubtype());
         res.put("creation_date", this.getCreation_date().getTime());
         res.put("team_id", this.getTeam().getDb_id());
         res.put("channel_id", this.getChannel().getDb_id());
