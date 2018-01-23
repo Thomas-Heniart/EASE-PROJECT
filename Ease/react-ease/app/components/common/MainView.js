27d7ae1318bdc70daa29c1c495984e855854bd67
Thomas Heniart <heniart.thomas@gmail.com>
2018-01-23 16:28:37 +0100

    Commit to checkout

--- Ease/react-ease/app/components/common/MainView.js
+++ Ease/react-ease/app/components/common/MainView.js
@@ -4,36 +4,122 @@ import TeamsPreview from '../teams/TeamsPreview';
 import Catalog from '../catalog/Catalog';
 import Dashboard from "../dashboard/Dashboard";
 import Settings from '../settings/Settings';
-import { Switch, Route } from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
 import HTML5Backend from 'react-dnd-html5-backend';
-import { DragDropContext } from 'react-dnd';
+import {DragDropContext} from 'react-dnd';
 import CustomDragLayer from "../dashboard/CustomDragLayer";
 import NotificationBoxContainer from "../common/NotificationBoxContainer";
+import Joyride from 'react-joyride';
+
 var SimpleTeamCreationView = require('../teams/SimpleTeamCreationView');
 import {connect} from "react-redux";
 import {showExtensionDownloadModal} from "../../actions/modalActions";
 
 @connect()
 class MainView extends Component {
-  componentDidMount(){
-    if (!document.querySelector('#new_ease_extension')){
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      joyrideOverlay: true,
+      joyrideType: 'continuous',
+      isReady: true,
+      isRunning: true,
+      stepIndex: 0,
+      steps: [],
+      selector: '',
+    };
+  }
+
+  addSteps(steps) {
+    let newSteps = steps;
+
+    if (!Array.isArray(newSteps)) {
+      newSteps = [newSteps];
+    }
+
+    if (!newSteps.length) {
+      return;
+    }
+
+    // Force setState to be synchronous to keep step order.
+    this.setState(currentState => {
+      currentState.steps = currentState.steps.concat(newSteps);
+      return currentState;
+    });
+  }
+
+  addTooltip(data) {
+    this.joyride.addTooltip(data);
+  }
+
+  next() {
+    this.joyride.next();
+  }
+
+  callback(data) {
+    console.log('%ccallback', 'color: #47AAAC; font-weight: bold; font-size: 13px;'); //eslint-disable-line no-console
+    console.log(data); //eslint-disable-line no-console
+
+    this.setState({
+      selector: data.type === 'tooltip:before' ? data.step.selector : '',
+    });
+  }
+
+  componentDidMount() {
+    if (!document.querySelector('#new_ease_extension')) {
       this.props.dispatch(showExtensionDownloadModal({active: true}));
     }
   }
+
   render() {
+    const {
+      isReady,
+      isRunning,
+      joyrideOverlay,
+      joyrideType,
+      selector,
+      stepIndex,
+      steps,
+    } = this.state;
+
     return (
-        <div class="display-flex flex_direction_column full_flex" style={{flexFlow: 'column-reverse'}}>
-          <Switch>
-            <Route path='/main/teamsPreview' component={TeamsPreview}/>
-            <Route path='/main/simpleTeamCreation' component={SimpleTeamCreationView}/>
-            <Route path='/main/catalog' component={Catalog}/>
-            <Route path='/main/settings' component={Settings}/>
-            <Route path='/main/dashboard' component={Dashboard}/>
-          </Switch>
-          <EaseHeader/>
-          <CustomDragLayer/>
-          <NotificationBoxContainer/>
-        </div>
+      <div class="display-flex flex_direction_column full_flex" style={{flexFlow: 'column-reverse'}}>
+        <Joyride
+          ref={c => {this.joyride = c;}}
+          callback={this.callback}
+          debug={false}
+          disableOverlay={false}
+          locale={{
+            back: (<span>Back</span>),
+            close: (<span>Close</span>),
+            last: (<span>Last</span>),
+            next: (<span>Next</span>),
+            skip: (<span>Skip</span>),
+          }}
+          run={isRunning}
+          showOverlay={joyrideOverlay}
+          showSkipButton={true}
+          showStepsProgress={true}
+          stepIndex={stepIndex}
+          steps={steps}
+          type={joyrideType}
+        />
+        <Switch>
+          <Route path='/main/teamsPreview' component={TeamsPreview}/>
+          <Route path='/main/simpleTeamCreation' component={SimpleTeamCreationView}/>
+          <Route path='/main/catalog' component={Catalog}/>
+          <Route path='/main/settings' component={Settings}/>
+          <Route path='/main/dashboard' render={(props) => <Dashboard joyrideType={joyrideType}
+                                                                      joyrideOverlay={joyrideOverlay}
+                                                                      addSteps={this.addSteps}
+                                                                      addTooltip={this.addTooltip}
+                                                                      {...this.props}/>}/>
+        </Switch>
+        <EaseHeader/>
+        <CustomDragLayer/>
+        <NotificationBoxContainer/>
+      </div>
     )
   }
 }
--- Ease/react-ease/app/components/dashboard/Dashboard.js
+++ Ease/react-ease/app/components/dashboard/Dashboard.js
@@ -11,6 +11,7 @@ import HTML5Backend from 'react-dnd-html5-backend';
 import { DragDropContext } from 'react-dnd';
 import CustomDragLayer from "./CustomDragLayer";
 import { DropTarget, DragSource } from 'react-dnd';
+import Joyride from 'react-joyride';
 import withScrolling from 'react-dnd-scrollzone';
 
 const ScrollingComponent = withScrolling('div');
@@ -67,15 +68,59 @@ class Dashboard extends Component {
         }, 3000);
       }
     }
+    this.props.addTooltip({
+      title: 'Standalone Tooltips',
+      text: '<h2 style="margin-bottom: 10px; line-height: 1.6">Now you can open tooltips independently!</h2>And even style them one by one!',
+      selector: '.app_wrapper',
+      position: 'bottom',
+      event: 'click',
+      style: {
+        backgroundColor: 'rgba(0, 0, 0, 0.8)',
+        borderRadius: 0,
+        color: '#fff',
+        mainColor: '#ff67b4',
+        textAlign: 'center',
+        width: '29rem',
+      }
+    });
+
   }
   componentWillMount() {
     document.title = "Ease.space";
   }
   render(){
     const {columns} = this.props.dashboard;
-
+    const {
+      isReady,
+      isRunning,
+      joyrideOverlay,
+      joyrideType,
+      selector,
+      stepIndex,
+      steps,
+    } = this.state;
     return (
         <div id="dashboard" class={classnames(this.props.background_picture ? 'ease-background' : null, this.state.scrolling ? 'scrolling': null, 'lite_scrollbar')}>
+          <Joyride
+            ref={c => (this.joyride = c)}
+            callback={this.callback}
+            debug={false}
+            disableOverlay={selector === '.card-tickets'}
+            locale={{
+              back: (<span>Back</span>),
+              close: (<span>Close</span>),
+              last: (<span>Last</span>),
+              next: (<span>Next</span>),
+              skip: (<span>Skip</span>),
+            }}
+            run={isRunning}
+            showOverlay={joyrideOverlay}
+            showSkipButton={true}
+            showStepsProgress={true}
+            stepIndex={stepIndex}
+            steps={steps}
+            type={joyrideType}
+          />
           <ScrollingComponent onScroll={this.onScroll} class="ui container fluid full_flex display_flex">
             {columns.map((column,idx) => {
               return (
