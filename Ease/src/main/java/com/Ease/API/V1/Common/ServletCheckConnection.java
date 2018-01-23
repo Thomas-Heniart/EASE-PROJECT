bfa569dbdbb7c1b9e950ba0e1ae722008dcb1b4e
Thomas Heniart <heniart.thomas@gmail.com>
2018-01-23 17:14:08 +0100

    Fix privateKey problem

--- Ease/src/main/java/com/Ease/API/V1/Common/ServletCheckConnection.java
+++ Ease/src/main/java/com/Ease/API/V1/Common/ServletCheckConnection.java
@@ -11,6 +11,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/bz")
@@ -20,16 +21,24 @@ public class ServletCheckConnection extends HttpServlet {
             PostServletManager sm = new PostServletManager(this.getClass().getName(), request, response, true);
             try {
                 JSONObject res = new JSONObject();
-                try {
-                    sm.needToBeConnected();
-                    res.put("connected", true);
-                    sm.setSuccess(res);
-                } catch (HttpServletException e) {
-                    if (e.getHttpStatus() == HttpStatus.AccessDenied) {
+                HttpSession session = sm.getSession();
+                Integer user_id = (Integer) session.getAttribute("user_id");
+                if (user_id == null)
+                    res.put("connected", false);
+                else {
+                    if (sm.getUserProperties(user_id).get("keyUser") == null)
                         res.put("connected", false);
-                        sm.setSuccess(res);
-                    } else
-                        throw e;
+                    else {
+                        try {
+                            sm.needToBeConnected();
+                            res.put("connected", true);
+                        } catch (HttpServletException e) {
+                            if (e.getHttpStatus() == HttpStatus.AccessDenied)
+                                res.put("connected", false);
+                            else
+                                throw e;
+                        }
+                    }
                 }
                 sm.setSuccess(res);
             } catch (Exception e) {
