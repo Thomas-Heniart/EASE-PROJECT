5555d5670c5beaed3f255d5f1974c8c887a45a5f
Thomas Heniart <heniart.thomas@gmail.com>
2018-01-30 13:29:35 +0100

    Tip on importation selection

--- Ease/react-ease/app/components/catalog/Importations.js
+++ Ease/react-ease/app/components/catalog/Importations.js
@@ -11,6 +11,8 @@ import {createProfile} from "../../actions/dashboardActions";
 import {createTeamChannel, addTeamUserToChannel} from "../../actions/channelActions";
 import {getLogo} from "../../utils/api"
 import { Segment, Button, Icon, TextArea, Dropdown, Form, Menu, Message, Input, Loader, Grid, Label} from 'semantic-ui-react';
+import Joyride from "react-joyride";
+import {setTipSeen} from "../../actions/commonActions";
 
 function json(fields, separator, csv, dispatch) {
   const array = csv.split('\n');
@@ -561,7 +563,8 @@ class DisplayAccounts extends React.Component {
                           onClose={this.openDropdown}
                           onBlur={this.closeOnBlur}
                           text={this.props.location}
-                          error={error !== ''}>
+                          error={error !== ''}
+                          id="importation_dropdown">
                   <Dropdown.Menu>
                     <Dropdown.Header><Icon name='user'/>Personal Space</Dropdown.Header>
                     {profiles}
@@ -582,12 +585,31 @@ class DisplayAccounts extends React.Component {
                     {teamsList}
                   </Dropdown.Menu>
                 </Dropdown>
+                {!this.props.user.status.tip_importation_seen &&
+                <Joyride
+                  steps={[{
+                    title: 'Organize your apps by sending them where you want to!',
+                    isFixed: true,
+                    selector:"#importation_dropdown",
+                    position: 'bottom'
+                  }]}
+                  locale={{ back: 'Back', close: 'Got it!', last: 'Got it!', next: 'Next', skip: 'Skip the tips' }}
+                  disableOverlay={true}
+                  run={true}
+                  callback={(action) => {
+                    if (action.type === 'finished')
+                      this.props.dispatch(setTipSeen({
+                        name: 'tip_importation_seen'
+                      }));
+                  }}
+                />}
               </div>
               <div className='div_accounts'>
                 <Message error hidden={error === ''} content={error} size='mini'/>
                 {listPending}
               </div>
               <Button
+                id="import_button"
                 positive
                 loading={loadingSending}
                 disabled={accountsPending.length < 1 || (selectedProfile === -1 && selectedRoom === -1) || loadingSending}
@@ -678,7 +700,8 @@ class ErrorAccounts extends React.Component {
   websites: store.catalog.websites,
   dashboard: store.dashboard,
   profiles: store.dashboard.profiles,
-  teams: store.teams
+  teams: store.teams,
+  user: store.common.user
 }))
 class Importations extends React.Component {
   constructor(props) {
