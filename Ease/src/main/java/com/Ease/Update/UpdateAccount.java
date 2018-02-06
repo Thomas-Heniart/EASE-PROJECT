21f8071f33ccbb1b60b9e5ee16f3b16c9d82107e
Thomas Heniart <heniart.thomas@gmail.com>
2018-02-06 18:29:38 +0100

    Start SQL script and Java POJO

---
+++ Ease/src/main/java/com/Ease/Update/UpdateAccount.java
@@ -0,0 +1,59 @@
+package com.Ease.Update;
+
+import com.Ease.Utils.HttpServletException;
+import org.json.JSONObject;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "UPDATE_ACCOUNT")
+public class UpdateAccount {
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Long id;
+
+    @OneToMany(mappedBy = "updateAccount", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<UpdateAccountInformation> updateAccountInformationSet = new HashSet<>();
+
+    public UpdateAccount() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Set<UpdateAccountInformation> getUpdateAccountInformationSet() {
+        return updateAccountInformationSet;
+    }
+
+    public void setUpdateAccountInformationSet(Set<UpdateAccountInformation> updateAccountInformationSet) {
+        this.updateAccountInformationSet = updateAccountInformationSet;
+    }
+
+    public void addUpdateAccountInformation(UpdateAccountInformation updateAccountInformation) {
+        this.getUpdateAccountInformationSet().add(updateAccountInformation);
+    }
+
+    public void removeUpdateAccountInformation(UpdateAccountInformation updateAccountInformation) {
+        this.getUpdateAccountInformationSet().remove(updateAccountInformation);
+    }
+
+    public void decipher(String private_key) throws HttpServletException {
+        for (UpdateAccountInformation updateAccountInformation : this.getUpdateAccountInformationSet())
+            updateAccountInformation.decipher(private_key);
+    }
+
+    public JSONObject getJson() {
+        JSONObject res = new JSONObject();
+        this.getUpdateAccountInformationSet().forEach(updateAccountInformation -> res.put(updateAccountInformation.getName(), updateAccountInformation.getDeciphered_value()));
+        return res;
+    }
+}
