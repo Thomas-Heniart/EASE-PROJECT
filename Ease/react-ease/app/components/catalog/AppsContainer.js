bedbf897aceed63bbc1f3a5390d5eae14213c4fd
victorien caquant <vcaquant@student.42.fr>
2018-02-12 15:25:39 +0100

    [FRONT][CATALOG][UPDATES]: for fix bug

--- Ease/react-ease/app/components/catalog/AppsContainer.js
+++ Ease/react-ease/app/components/catalog/AppsContainer.js
@@ -12,11 +12,11 @@ const AppsContainer  = ({websites, title, openModal}) => {
         </h3>
         <Grid columns={4} className="logoCatalog">
           {websites.map((item) =>
-              <Grid.Column key={item.id} as='a' className="showSegment" onClick={e => openModal(item)}>
-                <Image src={item.logo}/>
-                <p>{item.name}</p>
-                <Icon name="add square"/>
-              </Grid.Column>
+            <Grid.Column key={item.id} as='a' className="showSegment" onClick={e => openModal(item)}>
+              <Image src={item.logo}/>
+              <p>{item.name}</p>
+              <Icon name="add square"/>
+            </Grid.Column>
           )}
         </Grid>
         <AddAnyApp query={""} focus={false}/>
