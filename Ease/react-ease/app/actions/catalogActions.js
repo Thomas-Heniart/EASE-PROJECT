d728d7c30f619c7a3b67939c27b4ec2817c5a0e4
Cleclemi28 <c.trofleau@gmail.com>
2018-02-12 16:23:07 +0100

    add new modal actions

--- Ease/react-ease/app/actions/catalogActions.js
+++ Ease/react-ease/app/actions/catalogActions.js
@@ -480,6 +480,35 @@ export function showAccountUpdateModal({state, resolve, reject, website, account
   }
 }
 
+export function newAccountUpdateModal(dispatch, website, account_information){
+    return new Promise((resolve, reject) => {
+        dispatch(showNewAccountUpdateModal({
+            state: true,
+            website: website,
+            account_information: account_information,
+            resolve: resolve,
+            reject: reject
+        }));
+    }).then(response => {
+        console.log('resolve')
+    }).catch(err => {
+        dispatch(showNewAccountUpdateModal({state: false}));
+    });
+}
+
+export function showNewAccountUpdateModal({state, resolve, reject, website, account_information}){
+    return {
+        type: 'SHOW_NEW_ACCOUNT_UPDATE_MODAL',
+        payload: {
+            active: state,
+            website: website,
+            account_information: account_information,
+            resolve: resolve,
+            reject: reject
+        }
+    }
+}
+
 export function deleteUpdate({id}){
   return (dispatch, getState) => {
     return delete_api.catalog.deleteUpdate({
