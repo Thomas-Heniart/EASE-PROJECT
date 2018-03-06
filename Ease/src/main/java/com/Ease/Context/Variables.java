8439e202414526d1c718cd3314ba3b6adedddcd6
Thomas Heniart <heniart.thomas@gmail.com>
2018-03-06 19:07:20 +0100

    css version

--- Ease/src/main/java/com/Ease/Context/Variables.java
+++ Ease/src/main/java/com/Ease/Context/Variables.java
@@ -14,13 +14,11 @@ public class Variables {
     public static final String RGDP_EBOOK_PATH = System.getenv("EASE_RESOURCES_FOLDER");
 
     //Prod
-    /*
     public static final String ENVIRONNEMENT = "Prod";
     public static final String KEYS_PATH = "/opt/tomcat/webapps/ROOT/WEB-INF/classes/com/Ease/Utils/Crypto/privateKeys.txt";
     public static final String URL_PATH = "https://ease.space/";
     public static final String DATABASE_IP = "127.0.0.1";
     public static final String STRIPE_API_KEY = "sk_live_P9qlj7iut6cpNkb8hfY4S43M";
-    */
     //Préprod
     /*
     public static final String ENVIRONNEMENT = "Préprod";
@@ -38,9 +36,11 @@ public class Variables {
     public static final String STRIPE_API_KEY ="sk_test_4Qqw6xcv7VQDmXBS5CZ9rz5T";
     */
     //Toto
+    /*
     public static final String ENVIRONNEMENT = "PC de Toto";
     public static final String KEYS_PATH = "/Users/thomas/EASE-PROJECT/Ease/src/com/Ease/Utils/Crypto/privateKeys.txt";
     public static final String URL_PATH = "https://localhost:8443/";
     public static final String DATABASE_IP = "127.0.0.1";
     public static final String STRIPE_API_KEY = "sk_test_4Qqw6xcv7VQDmXBS5CZ9rz5T";
+    */
 }
