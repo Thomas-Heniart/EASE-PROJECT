8e8c73dca8de744c9c20880e1c8bb27e2a345212
Thomas Heniart <heniart.thomas@gmail.com>
2018-03-13 16:45:10 +0100

    Fix and minor modification for onboarding stats

--- Ease/WebContent/js/backOffice/admin.js
+++ Ease/WebContent/js/backOffice/admin.js
@@ -282,6 +282,32 @@ $(document).ready(function () {
           }, (data) => {
             if (!!onboarding_chart)
               onboarding_chart.destroy();
+            data.option = {
+              scales: {
+                yAxes: [{
+                  ticks: {
+                    beginAtZero:true
+                  }
+                }]
+              }
+            };
+            data.data.datasets[0].backgroundColor = [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+            ];
+            data.data.datasets[0].borderColor = [
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+            ];
+            data.data.datasets[0].borderWidth = 1;
             onboarding_chart = new Chart(graph, data);
           });
           break;
