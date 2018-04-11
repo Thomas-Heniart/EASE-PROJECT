d50efeb23f3f2015d426b01ff8ce54b4fa431d9f
Thomas Heniart <heniart.thomas@gmail.com>
2018-04-11 17:58:17 +0200

    Database script, update entity and must finish post

---
+++ Ease/src/main/java/com/Ease/API/V1/Common/ServletPopupMagicLinkSeen.java
@@ -0,0 +1,35 @@
+package com.Ease.API.V1.Common;
+
+import com.Ease.User.User;
+import com.Ease.Utils.Servlets.PostServletManager;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/api/v1/common/PopupMagicLinkSeen")
+public class ServletPopupMagicLinkSeen extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PostServletManager sm = new PostServletManager(this.getClass().getName(), request, response, true);
+        try {
+            sm.needToBeConnected();
+            Boolean res = sm.getBooleanParam("response", true, false);
+            User user = sm.getUser();
+            user.getUserStatus().setPopupChooseMagicAppsSeen(true);
+            sm.saveOrUpdate(user.getUserStatus());
+            sm.setSuccess(user.getJson());
+        } catch (Exception e) {
+            sm.setError(e);
+        }
+        sm.sendResponse();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+        rd.forward(request, response);
+    }
+}
---
+++ tools/es-218.sql
@@ -0,0 +1,12 @@
+ALTER TABLE status
+  ADD COLUMN popup_choose_magic_apps_seen TINYINT(1) UNSIGNED NOT NULL DEFAULT 1;
+UPDATE status
+SET popup_choose_magic_apps_seen = 0
+WHERE registered = 1;
+
+CREATE TABLE MAGIC_APPS_SURVEY (
+  id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  user_id INT(10) UNSIGNED NOT NULL,
+  response TINYINT(1) UNSIGNED NOT NULL,
+  PRIMARY KEY (id)
+);
