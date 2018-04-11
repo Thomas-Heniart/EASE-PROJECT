4eeb5206a8738e910e86495a02024f4b82f954a2
Thomas Heniart <heniart.thomas@gmail.com>
2018-04-11 16:09:53 +0200

    Fix problem in backoffice

--- tools/Utils.sql
+++ tools/Utils.sql
@@ -238,8 +238,28 @@ SELECT
   day_of_year,
   COUNT(*) AS clicks
 FROM ease_tracking.EASE_EVENT
+WHERE (name LIKE 'PasswordUsed' OR name LIKE 'PasswordUser' AND user_id IN (SELECT user_id
+                                                                           FROM ease.teamUsers
+                                                                           WHERE team_id = 312 AND user_id IS NOT NULL)
+GROUP BY year, day_of_year
+ORDER BY year, day_of_year;
+
+SELECT *
+FROM ease_tracking.EASE_EVENT
 WHERE name LIKE 'PasswordUsed' OR name LIKE 'PasswordUser' AND user_id IN (SELECT user_id
                                                                            FROM ease.teamUsers
-                                                                           WHERE team_id = 5)
+                                                                           WHERE team_id = 312 AND user_id IS NOT NULL);
+
+SELECT
+  year,
+  day_of_year,
+  COUNT(*) AS clicks
+FROM (
+       SELECT
+         year,
+         day_of_year,
+         id
+       FROM ease_tracking.EASE_EVENT
+       WHERE name LIKE ('PasswordUsed' OR name LIKE 'PasswordUser') AND user_id = 3411) AS t
 GROUP BY year, day_of_year
-ORDER BY YEAR, day_of_year;
+ORDER BY year, day_of_year;
