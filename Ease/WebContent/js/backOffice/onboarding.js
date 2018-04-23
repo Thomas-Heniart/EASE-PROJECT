6a56230213aa681d5a030e5e7ac9d84e915118db
Thomas Heniart <heniart.thomas@gmail.com>
2018-04-23 11:41:11 +0200

    Some teams stats

--- Ease/WebContent/js/backOffice/onboarding.js
+++ Ease/WebContent/js/backOffice/onboarding.js
@@ -1,8 +1,14 @@
 let beforeOnboardingCanvas = $("#beforeOnboardingChart");
 let beforeOnboardingChart;
 
+let afterOnboardingCanvas = $("#afterOnboardingChart");
+let afterOnboardingChart;
+
 const buildBeforeOnboardingChart = (data) => {
   const options = {
+    legend: {
+      display: false
+    },
     scales: {
       yAxes: [{
         ticks: {
@@ -49,9 +55,71 @@ const buildBeforeOnboardingChart = (data) => {
   })
 };
 
+const buildAfterOnboardingChart = (data) => {
+  const options = {
+    legend: {
+      display: false
+    },
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  };
+  let datasets = [];
+  datasets.push({
+    backgroundColor: [
+      'rgba(175, 210, 233, 0.7)',
+      'rgba(154, 113, 151, 0.7)',
+      'rgba(157, 150, 184, 0.7)',
+      'rgba(157, 150, 184, 0.7)',
+      'rgba(157, 150, 184, 0.7)',
+      'rgba(157, 150, 184, 0.7)'
+    ],
+    borderColor: [
+      'rgba(175, 210, 233, 1)',
+      'rgba(154, 113, 151, 1)',
+      'rgba(157, 150, 184, 1)',
+      'rgba(157, 150, 184, 1)',
+      'rgba(157, 150, 184, 1)',
+      'rgba(157, 150, 184, 1)',
+      'rgba(157, 150, 184, 1)'
+    ],
+    borderWidth: 1,
+    data: data
+  });
+  if (!!afterOnboardingChart)
+    afterOnboardingChart.destroy();
+  afterOnboardingChart = new Chart(afterOnboardingCanvas, {
+    type: 'bar',
+    label: 'After creation',
+    data: {
+      labels: [
+        'Teams created',
+        'Active teams',
+        '0->10 tools',
+        '11->20 tools',
+        '21->30 tools',
+        '30+ tools'
+      ],
+      datasets: datasets
+    },
+    options: options
+  })
+};
+
+
 $(document).ready(() => {
   ajaxHandler.get("/api/v1/admin/onboarding-before-creation-chart-data", {}, () => {
   }, (data) => {
+    $(".segment[data-tab=before_onboarding]").removeClass("loading");
     buildBeforeOnboardingChart(data);
+  });
+  ajaxHandler.get("/api/v1/admin/onboarded-teams-chart-data", {}, () => {
+  }, (data) => {
+    $(".segment[data-tab=after_onboarding]").removeClass("loading");
+    buildAfterOnboardingChart(data);
   })
 });
